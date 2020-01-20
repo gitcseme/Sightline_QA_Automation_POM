bba169d1c99cd3dff796cb725d9d80eaaf7fe3b7
Suresh Bavihalli <Suresh.Bavihalli@consilio.com>
2020-01-20 15:59:01 +0530

    Update BaseClass.java

--- sightline/src/main/java/pageFactory/BaseClass.java
+++ sightline/src/main/java/pageFactory/BaseClass.java
@@ -166,15 +166,13 @@ public class BaseClass {
 				getProjectNames().Visible()  ;}}), 10000);
 		driver.scrollPageToTop();
 		//Select project if required one is not seletced
-		if(!getProjectNames().getText().equalsIgnoreCase(Input.projectName)) {
-		
 		getProjectNames().waitAndClick(5);
 		  
     	driver.WaitUntil((new Callable<Boolean>() {public Boolean call(){return 
     			getSelectProject().Visible()  ;}}), 10000);
 		getSelectProject().waitAndClick(5);
 		driver.waitForPageToBeReady();
-		}
+	
 	}
     
  /*   public void BckTaskClick() {
