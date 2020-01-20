0fe29383ab21d29a64a8e7b5fd21f52c4d1458fe
SBavihalli <SBavihalli@INLTP--SBAVI.consilio.com>
2020-01-20 13:12:01 +0530

    now landing to search page

--- sightline/src/main/java/testScriptsRegression/ToolTipsInfos.java
+++ sightline/src/main/java/testScriptsRegression/ToolTipsInfos.java
@@ -44,6 +44,8 @@ public class ToolTipsInfos {
 		System.out.println("******Execution started for "+this.getClass().getSimpleName()+"********");
     	
 		//Open browser
+		Input in = new Input();
+		in.loadEnvConfig();
 		softAssertion= new SoftAssert();
 		driver = new Driver();
 		bc = new BaseClass(driver);
@@ -92,7 +94,7 @@ public class ToolTipsInfos {
 		System.out.println("------------------------------------------");
 	    System.out.println("Executing method : " + testMethod.getName());       
 	 }
-     @AfterMethod(alwaysRun = true)
+   //  @AfterMethod(alwaysRun = true)
 	 public void takeScreenShot(ITestResult result) {
  	 if(ITestResult.FAILURE==result.getStatus()){
  		 
@@ -109,7 +111,7 @@ public class ToolTipsInfos {
      }
      
    
-	@AfterClass(alwaysRun = true)
+	//@AfterClass(alwaysRun = true)
 	public void close(){
 		try{ 
 			lp.logout();
