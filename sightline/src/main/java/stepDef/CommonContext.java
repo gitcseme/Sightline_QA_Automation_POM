d834522b5fb80d614877e602d795787929bb3e14
Jeremiah De Leon <jeremiahdeleon@Jeremiahs-MacBook-Pro.local>
2020-10-29 16:40:56 -0700

    completed test case 4993

--- sightline/src/main/java/stepDef/CommonContext.java
+++ sightline/src/main/java/stepDef/CommonContext.java
@@ -1,8 +1,19 @@
 package stepDef;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
 import java.util.concurrent.Callable;
 
 import automationLibrary.Driver;
@@ -313,6 +324,7 @@ public class CommonContext {
 		driver.waitForPageToBeReady();
 								
 	}
+    
    
     public HashMap close_browser(boolean scriptState, HashMap dataMap) {
 		try{ 
