0ce654092d9df4d52e3f7baec4c5d610bd6cd614
vinod T <thatikonda.vinod@consilio.com>
2021-09-13 12:42:54 +0530

    Consolidated code

--- sightline/src/main/java/testScriptsRegression/SystemTemplate_Regression.java
+++ sightline/src/main/java/testScriptsRegression/SystemTemplate_Regression.java
@@ -1,5 +1,4 @@
 package testScriptsRegression;
-
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
@@ -24,89 +23,77 @@ public class SystemTemplate_Regression {
 
 	@BeforeClass(alwaysRun = true)
 	public void before() throws ParseException, InterruptedException, IOException {
-		System.out.println("******Execution started for " + this.getClass().getSimpleName() + "********");
-		
-		
-		driver = new Driver();
+		System.out.println("******Execution started for "+this.getClass().getSimpleName()+"********");
+	 	driver = new Driver();
 		lp = new LoginPage(driver);
 		bc = new BaseClass(driver);
-
+		
 	}
-
-	@Test(priority = 1, groups = { "smoke", "regression" })
-	public void provisionedTags_8633_1() throws InterruptedException {
+	
+	@Test(priority =1,groups={"smoke","regression"})
+	public void provisionedTags_8633_1() throws  InterruptedException {
 		lp.loginToSightLine(Input.pa1userName, Input.pa1password);
 		tnfpage = new TagsAndFoldersPage(driver);
-		bc.stepInfo("Test case Id:RPMXCON-52910-provisionedTags");
-
-		tnfpage.provisionedTags();
-		bc.passedStep("Verified provisioned Tags are available under Default Tags ");
-		lp.logout();
-
+	    tnfpage.provisionedTags();
+	    lp.logout();
 	}
-
-	@Test(priority = 2, groups = { "smoke", "regression" })
+	
+	@Test(priority =2,groups={"smoke","regression"})
 	public void provisionedTags_8633_2() throws InterruptedException {
 		lp.loginToSightLine(Input.rmu1userName, Input.rmu1password);
 		bc.stepInfo("Test case Id:RPMXCON-52910-provisionedTagsAsRMU");
-
-		tnfpage = new TagsAndFoldersPage(driver);
+        tnfpage = new TagsAndFoldersPage(driver);
 		tnfpage.provisionedTagsAsRMU();
 		bc.passedStep("Verified provisioned Tags are available under Default Tags as RMU User");
 		lp.logout();
-
 	}
-
-	@Test(priority = 3, groups = { "smoke", "regression" })
+	
+	@Test(priority =3,groups={"smoke","regression"})
 	public void tagsClassifications_8657_1() throws InterruptedException {
 		lp.loginToSightLine(Input.pa1userName, Input.pa1password);
 		bc.stepInfo("Test case Id:RPMXCON-52927-tagsClassifications");
-
-		tnfpage = new TagsAndFoldersPage(driver);
+        tnfpage = new TagsAndFoldersPage(driver);
 		tnfpage.tagsClassifications();
 		bc.passedStep("Verified Tag Classification under All Tags");
 		lp.logout();
 
 	}
-
-	 @Test(priority =4,groups={"smoke","regression"})
+	
+	@Test(priority =4,groups={"smoke","regression"})
 	public void tagsClassifications_8657_2() throws InterruptedException {
 		lp.loginToSightLine(Input.rmu1userName, Input.rmu1password);
 		bc.stepInfo("Test case Id:RPMXCON-52927-tagsClassificationsAsRMU");
-
 		tnfpage = new TagsAndFoldersPage(driver);
 		tnfpage.tagsClassificationsAsRMU();
 		bc.passedStep("Verified Tag Classification under All Tags for RMU User");
 		lp.logout();
 
 	}
-
-	 @Test(priority =5,groups={"smoke","regression"})
+	
+	@Test(priority =5,groups={"smoke","regression"})
 	public void layerAnnotations_8660_1() throws InterruptedException {
 		lp.loginToSightLine(Input.pa1userName, Input.pa1password);
 		bc.stepInfo("Test case Id:RPMXCON-52930-layerAnnotations");
-
-		tnfpage = new TagsAndFoldersPage(driver);
+     	tnfpage = new TagsAndFoldersPage(driver);
 		tnfpage.layerAnnotations();
 		bc.passedStep("Verified provisioned Annotation Layer is available in Default Security Group");
 		lp.logout();
 
 	}
-
+	
 	@Test(priority =6,groups={"smoke","regression"})
 	public void layerAnnotations_8660_2() throws InterruptedException {
 		lp.loginToSightLine(Input.rmu1userName, Input.rmu1password);
 		bc.stepInfo("Test case Id:RPMXCON-52930-layerAnnotationsAsRMU");
-
 		tnfpage = new TagsAndFoldersPage(driver);
 		tnfpage.layerAnnotationsAsRMU();
 		bc.passedStep("Verified provisioned Annotation Layer is available in the Project as RMU User");
 		lp.logout();
 
 	}
-
-	 @Test(priority =7,groups={"smoke","regression"})
-	public void provisionedFolder_8661_1() throws InterruptedException {
+	
+	@Test(priority =7,groups={"smoke","regression"})
+	public void provisionedFolder_8661_1() throws  InterruptedException {
 		lp.loginToSightLine(Input.pa1userName, Input.pa1password);
 		bc.stepInfo("Test case Id:RPMXCON-52931-provisionedFolder");
 
@@ -116,95 +103,89 @@ public class SystemTemplate_Regression {
 		lp.logout();
 
 	}
-
+	
 	@Test(priority =8,groups={"smoke","regression"})
-	public void provisionedFolder_8661_2() throws InterruptedException {
+	public void provisionedFolder_8661_2() throws  InterruptedException {
 		lp.loginToSightLine(Input.rmu1userName, Input.rmu1password);
 		bc.stepInfo("Test case Id:RPMXCON-52931-provisionedFolderAsRMU");
-
 		tnfpage = new TagsAndFoldersPage(driver);
 		tnfpage.provisionedFolderAsRMU();
 		bc.passedStep("Verified Productions is available under under All Folders as RMU User ");
 		lp.logout();
 
 	}
-
-@Test(priority =9,groups={"smoke","regression"})
-	public void redactionTags_8663_1() throws InterruptedException {
+	
+	@Test(priority =9,groups={"smoke","regression"})
+	public void redactionTags_8663_1() throws  InterruptedException {
 		lp.loginToSightLine(Input.pa1userName, Input.pa1password);
 		bc.stepInfo("Test case Id:RPMXCON-52933-redactionTags");
-
 		tnfpage = new TagsAndFoldersPage(driver);
 		tnfpage.redactionTags();
 		bc.passedStep("Verified provisioned Redaction Tags are available under Default Security Group ");
 		lp.logout();
 
 	}
-
-	 @Test(priority =10,groups={"smoke","regression"})
-	public void redactionTags_8663_2() throws InterruptedException {
+	
+	@Test(priority =10,groups={"smoke","regression"})
+	public void redactionTags_8663_2() throws  InterruptedException {
 		lp.loginToSightLine(Input.rmu1userName, Input.rmu1password);
 		bc.stepInfo("Test case Id:RPMXCON-52933-redactionTagsAsRMU");
-
 		tnfpage = new TagsAndFoldersPage(driver);
 		tnfpage.redactionTagsAsRMU();
 		bc.passedStep("Verified provisioned Redaction Tags are available in the Project as RMU User");
 		lp.logout();
 
 	}
-
-	 @Test(priority =11,groups={"smoke","regression"})
+	
+	@Test(priority =11,groups={"smoke","regression"})
 	public void provisionedFolderGroup_9028() throws InterruptedException {
 		lp.loginToSightLine(Input.pa1userName, Input.pa1password);
 		bc.stepInfo("Test case Id:RPMXCON-53129-provisionedFolderGroup");
-
 		tnfpage = new TagsAndFoldersPage(driver);
 		tnfpage.provisionedFolderGroup();
 		bc.passedStep("Productions is available under Default Security Group");
 		lp.logout();
 
 	}
-
-	 @Test(priority =12,groups={"smoke","regression"})
+	
+	@Test(priority =12,groups={"smoke","regression"})
 	public void codingForm_8719() throws InterruptedException {
 		lp.loginToSightLine(Input.rmu1userName, Input.rmu1password);
 		bc.stepInfo("Test case Id:RPMXCON-52971-codingForm");
-
 		cf = new CodingForm(driver);
 		cf.codingForm();
 		bc.passedStep("Verified provisioned CF is available in the Project");
 		lp.logout();
 
 	}
-
-	 @Test(priority =13,groups={"smoke","regression"})
+	
+	@Test(priority =13,groups={"smoke","regression"})
 	public void codingFormTagsOrder_8720() throws InterruptedException {
 		lp.loginToSightLine(Input.rmu1userName, Input.rmu1password);
 		bc.stepInfo("Test case Id:RPMXCON-52972-codingFormTagsOrder");
 		cf = new CodingForm(driver);
 		cf.codingFormTagsOrder();
 		bc.passedStep("Verified provisioned CF with Specific Tags and Order is  available in the Project- RMU");
-		
 		lp.logout();
 	}
-
+	
+	
 	@AfterMethod(alwaysRun = true)
-	public void takeScreenShot(ITestResult result) {
-		if (ITestResult.FAILURE == result.getStatus()) {
-			Utility bc = new Utility(driver);
-			bc.screenShot(result);
-		}
+	 public void takeScreenShot(ITestResult result) {
+	 if(ITestResult.FAILURE==result.getStatus()){
+		Utility bc = new Utility(driver);
+		bc.screenShot(result);
 	}
-
-	@AfterClass(alwaysRun = true)
-	public void close() {
-		try {
+	}
+	 @AfterClass(alwaysRun = true)
+	 public void close(){
+		try{ 
 			lp.logout();
-			// lp.quitBrowser();
-		} finally {
-			lp.quitBrowser();
-
-		}
+		     //lp.quitBrowser();	
+			}finally {
+				lp.quitBrowser();
+			
+			}
 		LoginPage.clearBrowserCache();
 	}
 
