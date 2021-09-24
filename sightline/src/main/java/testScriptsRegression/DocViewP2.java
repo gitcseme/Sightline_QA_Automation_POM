867b7680cb2399c78552934eb8910800d148ef4b
Deeksha.NLS <Deeksha.NLS@LNT-HZ-NP-SQA15.consilio.com>
2021-09-24 17:46:16 +0530

    input commented

--- sightline/src/main/java/testScriptsRegression/DocViewP2.java
+++ sightline/src/main/java/testScriptsRegression/DocViewP2.java
@@ -52,8 +52,7 @@ import testScriptsSmoke.Input;
 	public void preCondition() throws InterruptedException, ParseException, IOException {
 		
 		System.out.println("******Execution started for "+this.getClass().getSimpleName()+"********");
-		Input in = new Input();
-		in.loadEnvConfig(); 
+		
 		//Open browser
 		driver = new Driver();
 		//Login as PA
