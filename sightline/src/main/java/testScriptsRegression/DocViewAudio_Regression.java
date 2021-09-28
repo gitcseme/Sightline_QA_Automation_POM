dd0bedf871dbfe316390aa33bdbf99b15af459dd
Deeksha.NLS <Deeksha.NLS@LNT-HZ-NP-SQA12.consilio.com>
2021-09-28 13:16:03 +0530

    added few more stepinfo

--- sightline/src/main/java/testScriptsRegression/DocViewAudio_Regression.java
+++ sightline/src/main/java/testScriptsRegression/DocViewAudio_Regression.java
@@ -1,5 +1,6 @@
 package testScriptsRegression;
 
+import java.io.IOException;
 import java.lang.reflect.Method;
 import java.text.ParseException;
 import org.testng.ITestResult;
@@ -9,6 +10,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import automationLibrary.Driver;
+import pageFactory.BaseClass;
 import pageFactory.CodingForm;
 import pageFactory.CommentsPage;
 import pageFactory.DocViewPage;
@@ -26,14 +28,19 @@ public class DocViewAudio_Regression {
 	String remark = "Audio Remark"+Utility.dynamicNameAppender();
 	String Comments = "Audio comment";
 	DocViewPage docview ;
+	BaseClass baseclass;
 	
 	@BeforeClass(alwaysRun = true)
-	public void before() throws InterruptedException {
+	public void before() throws InterruptedException, ParseException, IOException {
 		
 		System.out.println("******Execution started for "+this.getClass().getSimpleName()+"********");
+		 
 		driver = new Driver();
 		lp = new LoginPage(driver);
+		baseclass = new BaseClass(driver);
+		
 		lp.loginToSightLine(Input.rmu1userName, Input.rmu1password);
+	
 		
 		//add comment field
     	CommentsPage comments = new CommentsPage(driver);
@@ -52,59 +59,73 @@ public class DocViewAudio_Regression {
 		driver.getWebDriver().get(Input.url);
 		
 		lp.loginToSightLine(Input.rev1userName, Input.rev1password);
+		docview = new DocViewPage(driver);
 		SessionSearch search = new SessionSearch(driver);
 		search.audioSearch("morning", "North American English");
 		search.ViewInDocView();
-		docview = new DocViewPage(driver);
+		
+		
 
 	}
 	
-	  @Test(groups={"smoke","regression"})
+	  @Test(priority=1,groups={"smoke","regression"})
 		public void Verifyaudiodocthruhistorydropdown() throws ParseException, InterruptedException {
-		
+		  baseclass.stepInfo("Test case Id: RPMXCON- - Verifyaudiodocthruhistorydropdown ");
+		  baseclass.stepInfo("*****Verify Audio Doc through history dropdown*****");	
 			docview.audiodocthruhistorydropdown();
-			
+			baseclass.passedStep("*****Audio Doc verified through history dropdown successfully*****");		
 		}
 	
-	@Test(groups={"smoke","regression"})
+	@Test(priority=2,groups={"smoke","regression"})
 	public void AudioPersistentHits() throws ParseException, InterruptedException {
-	
+		baseclass.stepInfo("Test case Id: RPMXCON- - AudioPersistentHits");	
+		baseclass.stepInfo("*****Verify Audio persistent hits*****");
 		docview.VerifyAudiopersistentHit("morning");
+		baseclass.passedStep("*****Audio persistent hits verified successfully*****");
 		
 	}
 	
-	@Test(groups={"smoke","regression"})
+	@Test(priority=3,groups={"smoke","regression"})
 	public void addPlay() throws ParseException, InterruptedException {
-	
-		docview.playAudio();;
+		baseclass.stepInfo("Test case Id: RPMXCON-51054 - audio Play");	
+		baseclass.stepInfo("***** Verify Audio Play*****");
+		docview.playAudio();
+		baseclass.passedStep("*****Audio Played successfully*****");
 		
 	}
 
-	@Test(groups={"smoke","regression"})
+	@Test(priority=4,groups={"smoke","regression"})
 	public void addRemark() throws ParseException, InterruptedException {
-			
+		baseclass.stepInfo("Test case Id: RPMXCON-46859 - audio remark");	
+		baseclass.stepInfo("*****Add/delete Audio remark*****");
 		docview.audioRemark(remark);
+		baseclass.passedStep("*****Audio remark deleted and added successfully*****");
 	}
 	
-	@Test(groups={"smoke","regression"})
+	@Test(priority=5,groups={"smoke","regression"})
 	public void addComment() throws ParseException, InterruptedException {
-	
+		baseclass.stepInfo("Test case Id: RPMXCON- - audio add comment");
+		baseclass.stepInfo("*****Add Audio coments*****");
 		docview.audioComment(remark);
-		
+		baseclass.passedStep("*****Audio coments added successfully*****");
 	}
 	
 	
-	@Test(groups={"smoke","regression"})
+	@Test(priority=7,groups={"smoke","regression"})
 	public void addreduction() throws ParseException, InterruptedException {
-		
+		baseclass.stepInfo("Test case Id: RPMXCON-46859 - audio reduction");
+		baseclass.stepInfo("*****Verify Audio Reduction*****");
 		docview.audioReduction();
+		baseclass.passedStep("*****Audio Reduction successfull*****");
 	
 	}
 	
-	@Test(groups={"smoke","regression"})
+	@Test(priority=6,groups={"smoke","regression"})
 	public void AudioDownload() throws ParseException, InterruptedException {
-		
+		 baseclass.stepInfo("Test case Id: RPMXCON-51110 - AudioDownload");
+		 baseclass.stepInfo("*****Verify Audio download*****");
 		docview.audioDownload();
+		baseclass.passedStep("*****Audio download successfull*****");
 	
 	}
 	 @BeforeMethod
