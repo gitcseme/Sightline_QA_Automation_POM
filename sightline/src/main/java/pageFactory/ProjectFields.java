f4414f3fb11fff30f96888940f2d69809d9ea798
Deeksha.NLS <Deeksha.NLS@LNT-HZ-NP-SQA18.consilio.com>
2021-10-12 16:50:04 +0530

    few changes done

--- sightline/src/main/java/pageFactory/ProjectFields.java
+++ sightline/src/main/java/pageFactory/ProjectFields.java
@@ -26,7 +26,8 @@ public class ProjectFields {
 		public Element selectIsSearchable(){ return driver.FindElementByXPath("//div[@class='widget-body']//div[1]/div[7]/div[2]/label[1]/i[1]"); }
 		public Element filterfields() { return driver.FindElementByXPath("//input[@id='txtsearchField']"); }
 		public Element Applyfilter() { return driver.FindElementByXPath("//a[@id='btnAppyFilter']"); }
-		 public Element getfieldName(String fieldname){ return driver.FindElementByXPath("//td[contains(text(),'"+fieldname+"')]"); }
+		 public Element getfieldName(String fieldname){ return driver.FindElementByXPath("//table[@id='ProjectFieldsDataTable']//td[(text()='"+fieldname+"')]"); }
+		
 		
 		
 		
@@ -54,9 +55,6 @@ public class ProjectFields {
 	    	driver.getWebDriver().get(Input.url+ "ProjectFields/ProjectFieldsList");
 		    driver.waitForPageToBeReady();
 		    addcommentfield().Click();
-		    //Actions ac = new Actions(driver.getWebDriver());
-		    //ac.moveToElement(fieldname().getWebElement()).perform();
-		    //fieldname().VisibilityOfElementExplicitWait(fieldname(), 60);
 		    driver.WaitUntil((new Callable<Boolean>() {public Boolean call(){return 
 		    		enterfieldname().Visible()  ;}}), Input.wait30); 
 		    enterfieldname().SendKeys(fieldname);
@@ -69,25 +67,26 @@ public class ProjectFields {
 		    ProjectFieldSave().Click();
 	    }
 	    
-	   public String VerifyCustomfield(String fieldname) throws InterruptedException {
+	   public String VerifyCustomfield(String fieldname)  {
 		   driver.getWebDriver().get(Input.url+ "ProjectFields/ProjectFieldsList");
 		   driver.waitForPageToBeReady();
 		   filterfields().SendKeys(fieldname);
 		   Applyfilter().waitAndClick(10);
+		   getfieldName(fieldname).waitForElement(getfieldName(fieldname));
+		  try {
+		   getfieldName(fieldname).Present();
 		   
-		   try {
 		   String Fieldtext = getfieldName(fieldname).getText();
-		  
 		   System.out.println(Fieldtext+" Custom field is present");
 		   return Fieldtext;
-		   }
 		   
+		  }
 		  
-		   
-		   catch(Exception e) {
+		   catch(Exception e)
+		    {
 			   
-			  
-			   return null;
+			  String ret = "nofield";
+			   return ret;
 			  
 		   }
 		   
