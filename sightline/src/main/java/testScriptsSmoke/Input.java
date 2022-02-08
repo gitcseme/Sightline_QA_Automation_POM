ed12ef36015d9855063a24814c9d3ba7af28a06b
MalayalaBaskar.Seeni <MalayalaBaskar.Seeni@LNT-W10-SQAT-03.consilio.com>
2022-02-08 17:32:31 +0530

    Commiting Sprint 12 cases

--- sightline/src/main/java/testScriptsSmoke/Input.java
+++ sightline/src/main/java/testScriptsSmoke/Input.java
@@ -490,6 +490,8 @@ public class Input {
     
     //Added by Iyappan
 	public static String hiddenDocId;
+	// Added by Baskar
+	public static String oneHourAudio;
 	@BeforeSuite(alwaysRun = true)
 
 	public void loadEnvConfig() throws ParseException, InterruptedException, IOException {
@@ -984,6 +986,9 @@ public class Input {
 		conceptualSearchString1 = testData.getConceptualSearchString1();
 		//Added by Iyappan
 				hiddenDocId = testData.getHiddenDocId();
+		// Added by baskar
+		oneHourAudio=testData.getOneHourAudio();
+				
 
 		System.out.println("*****************************************************");
 		UtilityLog.info("*****************************************************");
