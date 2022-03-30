376829f1b1d32acdb4301a8bdeaaafd723bd050c
Sowndarya.Velraj <Sowndarya.Velraj@LVS-W10-DEVS082.ConsilioTest.com>
2022-03-30 14:43:43 +0530

    stabilization changes

--- sightline/src/main/java/pageFactory/SessionSearch.java
+++ sightline/src/main/java/pageFactory/SessionSearch.java
@@ -2921,7 +2921,15 @@ public class SessionSearch {
 		}
 
 		getBulkActionButton().waitAndClick(5);
-		Thread.sleep(2000);
+		
+		base.waitTime(2);
+		if(getView().isDisplayed()) {
+			driver.waitForPageToBeReady();
+			Actions act = new Actions(driver.getWebDriver());
+			act.moveToElement(getView().getWebElement()).build().perform();
+			}else {
+			System.out.println("View is not found");
+			}
 
 		driver.WaitUntil((new Callable<Boolean>() {
 			public Boolean call() {
