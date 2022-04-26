b2a2a87bc0bd5e1c7eb2979ba5378b84e20bde21
Sai.Duvvuru <Sai.Duvvuru@LVS-W10-DEVS092.ConsilioTest.com>
2022-04-26 09:23:54 +0530

    resolving errors in branch

--- sightline/src/main/java/testScriptsRegression/Production_Test_Regression_03.java
+++ sightline/src/main/java/testScriptsRegression/Production_Test_Regression_03.java
@@ -3,6 +3,7 @@ package testScriptsRegression;
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.reflect.Method;
