0b4bd511954d149358df85303c6e2f8c29d3b655
Sai.Duvvuru <Sai.Duvvuru@LVS-W10-DEVS092.ConsilioTest.com>
2022-05-19 16:20:21 +0530

    Rebasing Stabilised Code

--- sightline/src/main/java/testScriptsRegression/DocView_Regression6.java
+++ sightline/src/main/java/testScriptsRegression/DocView_Regression6.java
@@ -52,6 +52,16 @@ public class DocView_Regression6 {
 	SavedSearch savedsearch;
 
 	String assignmentName = "AAassignment" + Utility.dynamicNameAppender();
+	String productionname;
+	String prefixID = "A_" + Utility.dynamicNameAppender();
+	String suffixID = "_P" + Utility.dynamicNameAppender();
+	String folder;
+	String folder2;
+	String tag;
+
+	String random = "Test657394345"+ Utility.dynamicNameAppender();
+	String random1 = random;
+	String AnnotationLayerNew = Input.randomText + Utility.dynamicNameAppender();
 
 	@BeforeClass(alwaysRun = true)
 
@@ -334,6 +344,168 @@ public class DocView_Regression6 {
 			docView.verifyDocumentDownload();
 }
 	
+/**
+ * Author : Gopinath Created date: NA Modified date:08-09-2021 NA Modified by:
+ * Krishna TestCase id : 52238 -52239 Verify that Project Admin prints the
+ * document without redaction which is released to two different security groups
+ * and redaction added in first security group only with shared annotation layer
+ * Description : To Verify that Project Admin prints the document without
+ * redaction
+ */
+
+@Test(description = "RPMXCON-52238,RPMXCON-52239", groups={"regression"},priority = 5)
+public void VerifyProjectAdminPrintDocWithoutRedaction() throws Exception {
+	baseClass = new BaseClass(driver);
+	baseClass.stepInfo("Test case Id: RPMXCON-52238, RPMXCON-52239");
+	utility = new Utility(driver);
+	baseClass.stepInfo("#### To Verify that Project Admin prints the document without redaction ####");
+	loginPage = new LoginPage(driver);
+
+	baseClass.stepInfo("Login with project administrator");
+	loginPage.loginToSightLine(Input.pa1userName, Input.pa1password);
+	Reporter.log("Logged in as User: " + Input.pa1userName);
+
+	securityGroupsPage = new SecurityGroupsPage(driver);
+	docViewRedact = new DocViewRedactions(driver);
+	docViewRedact.createNewAnnotationLayer(AnnotationLayerNew);
+
+	baseClass.stepInfo("Add security group");
+	securityGroupsPage.navigateToSecurityGropusPageURL();
+	securityGroupsPage.AddSecurityGroup(random1);
+
+	baseClass.stepInfo("Selecting security group");
+	securityGroupsPage.navigateToSecurityGropusPageURL();
+	securityGroupsPage.selectSecurityGroup(random1);
+
+	baseClass.stepInfo("Selecting default annotation layer from annotation layer table");
+	securityGroupsPage.clickOnAnnotationLinkAndSelectAnnotation(AnnotationLayerNew);
+	securityGroupsPage.clickOnAnnotationLinkAndSelectAnnotation(AnnotationLayerNew);
+	securityGroupsPage.clickOnAnnotationLinkAndSelectAnnotation(AnnotationLayerNew);
+
+	baseClass.stepInfo("Selecting default reduction layer from reduction layer table");
+	securityGroupsPage.clickOnReductionTagAndSelectReduction("Default Redaction Tag");
+	securityGroupsPage.clickOnReductionTagAndSelectReduction("Default Redaction Tag");
+	securityGroupsPage.clickOnReductionTagAndSelectReduction("Default Redaction Tag");
+
+	SessionSearch sessionSearch = new SessionSearch(driver);
+
+	baseClass.stepInfo("Basic search with text input");
+	sessionSearch.basicContentSearch(Input.searchText);
+
+	baseClass.stepInfo("Bulk release to security group");
+	sessionSearch.bulkRelease(random1);
+
+	baseClass.stepInfo("Edit user by name in Users page");
+	DocViewRedactions docViewRedact = new DocViewRedactions(driver);
+	docViewRedact.assignAccessToSecurityGroups(random1);
+
+	loginPage.logout();
+
+	loginPage.loginToSightLine(Input.rmu2userName, Input.rmu2password);
+	UtilityLog.info("Logged in as User: " + Input.rmu2userName);
+
+	docViewMetaDataPage = new DocViewMetaDataPage(driver);
+
+	docViewMetaDataPage.selectSecurityGroup(random1);
+	baseClass.stepInfo("Select security group for RMU");
+
+	sessionSearch = new SessionSearch(driver);
+
+	baseClass.stepInfo("Basic meta data search");
+	sessionSearch.basicContentSearch(Input.searchText);
+	sessionSearch.addDocsMetCriteriaToActionBoard();
+
+	baseClass.stepInfo("Click on reduction button ");
+	docViewMetaDataPage.clickOnRedactAndRectangle();
+
+	baseClass.stepInfo("Set rectangle reduct in doc");
+	docViewMetaDataPage.redactbyrectangle(10, 15, Input.defaultRedactionTag);
+
+	loginPage.logout();
+
+	baseClass.stepInfo("Login with project administrator");
+	loginPage.loginToSightLine(Input.pa1userName, Input.pa1password);
+	Reporter.log("Logged in as User: " + Input.pa1userName);
+
+	sessionSearch = new SessionSearch(driver);
+
+	baseClass.stepInfo("Basic content search");
+	sessionSearch.basicContentSearch(Input.searchText);
+	sessionSearch.addDocsMetCriteriaToActionBoard();
+	DocViewPage docView = new DocViewPage(driver);
+	baseClass.stepInfo("Verify PDF Document tab opened in new tab");
+	docView.verifyDocumentDownload();
+//logout as PA		
+	loginPage.logout();
+}
+
+/**
+ * Author : Krishna Created date: NA Modified date: NA Modified by: Krishna
+ * TestCase id : 51990 Documents released to two different security groups and
+ * search with WP To Verify that Persistant hits when same keywords are added to
+ * different SG
+ */
+
+@Test(description = "RPMXCON-51990", groups={"regression"},priority = 6)
+public void verifyPersistentHitPanelRMU1RMU2() throws Exception {
+	loginPage.loginToSightLine(Input.rmu1userName, Input.rmu1password);
+	folder = "51990Default_" + Utility.dynamicNameAppender();
+	folder2 = "51990DefaultSec_" + Utility.dynamicNameAppender();
+	DocViewRedactions docViewRedact = new DocViewRedactions(driver);
+	WebDriverWait wait = new WebDriverWait(driver.getWebDriver(), 100);
+	baseClass.stepInfo("Test case Id: RPMXCON 51990");
+	SessionSearch sessionsearch = new SessionSearch(driver);
+	sessionsearch.basicContentSearch(Input.searchText);
+	baseClass.stepInfo("Search with keyword completed");
+	sessionsearch.bulkFolder(folder);
+	baseClass.stepInfo("Search documents released to bulk folder");
+	wait.until(ExpectedConditions.elementToBeClickable(docViewRedact.addNewSearchBtn().getWebElement()));
+	docViewRedact.addNewSearchBtn().waitAndClick(5);
+	wait.until(ExpectedConditions.elementToBeClickable(docViewRedact.clearShoppingCart().getWebElement()));
+	docViewRedact.clearShoppingCart().waitAndClick(5);
+	sessionsearch.switchToWorkproduct();
+	driver.scrollingToBottomofAPage();
+	sessionsearch.selectFolderInASwp(folder);
+	sessionsearch.serarchWP();
+	baseClass.stepInfo("Search with Work product completed");
+	sessionsearch.ViewInDocView();
+	docViewRedact.checkingPersistentHitPanel();
+	if (docViewRedact.persistantHitToggle().Displayed() && docViewRedact.persistantHitToggle().Enabled()) {
+		
+		baseClass.passedStep("The persistent hit panel is visible");
+	} else {
+		baseClass.failedStep("The persistent hit panel is NOT visible");
+	}
+	loginPage.logout();
+	loginPage.loginToSightLine(Input.rmu2userName, Input.rmu2password);
+
+	DocViewMetaDataPage docViewMetaDataPage = new DocViewMetaDataPage(driver);
+	docViewMetaDataPage.selectSecurityGroup(random1);
+	baseClass.stepInfo("RMU2 Assigned to new SG");
+	sessionsearch.basicContentSearch(Input.searchText);
+	baseClass.stepInfo("Search with Work product completed with RMU2");
+	sessionsearch.ViewInDocView();
+	docViewRedact.checkingPersistentHitPanel();
+	if (docViewRedact.persistantHitToggle().Displayed() && docViewRedact.persistantHitToggle().Enabled()) {
+		baseClass.passedStep("The persistent hit panel is visible");
+	} else {
+		baseClass.failedStep("The persistent hit panel is NOT visible");
+	}
+	loginPage.logout();
+	
+	// assigning back to default and deleting the created SG
+	
+	loginPage.loginToSightLine(Input.rmu2userName, Input.rmu2password);
+	baseClass.stepInfo("Select security group for RMU");
+	docViewRedact.selectsecuritygroup("Default Security Group");
+	docViewRedact.selectsecuritygroup("Default Security Group");
+	baseClass.stepInfo("RMU2 Assigned to Default Security Group");
+	loginPage.logout();
+	loginPage.loginToSightLine(Input.pa1userName, Input.pa1password);
+	securityGroupsPage = new SecurityGroupsPage(driver);
+	securityGroupsPage.deleteSecurityGroups(random1);
+	loginPage.logout();
+}
 	
 
 }
