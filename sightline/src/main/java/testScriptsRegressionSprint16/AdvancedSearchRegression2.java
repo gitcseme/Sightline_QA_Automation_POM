f02ef45de2486989b7ede9e6729ea87db7d662ac
Sai.Duvvuru <Sai.Duvvuru@LVS-W10-DEVS092.ConsilioTest.com>
2022-07-06 11:56:01 +0530

    Commiting RPMXCON-57083,57089,57078,57242 - Advanced search
    Changed
    AdvancedSearchRegression2
    Sessionsearch page

--- sightline/src/main/java/testScriptsRegressionSprint16/AdvancedSearchRegression2.java
+++ sightline/src/main/java/testScriptsRegressionSprint16/AdvancedSearchRegression2.java
@@ -45,6 +45,13 @@ public class AdvancedSearchRegression2 {
 		in.loadEnvConfig();
 
 	}
+	
+	@DataProvider(name = "Users")
+	public Object[][] Users() {
+		Object[][] users = { { Input.pa1userName, Input.pa1password }, { Input.rmu1userName, Input.rmu1password },
+				{ Input.rev1userName, Input.rev1password }, };
+		return users;
+	}
 
 	/**
 	 * @Author
@@ -270,6 +277,127 @@ public class AdvancedSearchRegression2 {
 
 		// logout
 		login.logout();
+	}
+	
+	@Test(description ="RPMXCON-57083", dataProvider = "Users", groups = { "regression" })
+	public void verifyNavigatesfrmAdvsearchsreentoDocViewScreen(String username, String password) throws InterruptedException {
+		BaseClass baseClass = new BaseClass(driver);
+		LoginPage loginPage = new LoginPage(driver);
+		SessionSearch sessionSearch = new SessionSearch(driver);
+		baseClass.stepInfo("Test case Id: RPMXCON-57083");
+		baseClass.stepInfo(
+				"Verify that User Navigates from Advanced Search result to Document View screen.");
+		loginPage.loginToSightLine(username, password);
+		driver.getWebDriver().get(Input.url + "Search/Searches");
+		sessionSearch.navigateToAdvancedSearchPage();
+		baseClass.stepInfo("Perform search with metadata");
+		sessionSearch.advancedMetaDataSearch(Input.metaDataName, null, Input.custodianName_allen, null);
+		
+		baseClass.waitForElement(sessionSearch.getDocsMetYourCriteriaLabel());
+		baseClass.dragAndDrop(sessionSearch.getDocsMetYourCriteriaLabel(), sessionSearch.getActionPad());
+		sessionSearch.verifySessionSearchPageUrl();
+		
+		sessionSearch.ViewInDocViews();
+		
+		String actualUrl = driver.getUrl();
+		String expUrl = Input.url + "DocumentViewer/DocView";
+		SoftAssert asserts = new SoftAssert();
+		asserts.assertEquals(expUrl, actualUrl);
+		baseClass.stepInfo("Successfully Navigated To DocView Scrren");
+		baseClass.passedStep("Verified - that User Navigates from Advanced Search result to Document View screen");
+				
+		loginPage.logout();
+	
+	}
+	
+	@Test(description ="RPMXCON-57089",dataProvider = "Users", groups = { "regression" })
+	public void verifyCopytoNewSearchinAdvanceSearch(String username, String password) throws InterruptedException {
+		BaseClass baseClass = new BaseClass(driver);
+		LoginPage loginPage = new LoginPage(driver);
+		SessionSearch sessionSearch = new SessionSearch(driver);
+		baseClass.stepInfo("Test case Id: RPMXCON-57089");
+		baseClass.stepInfo(
+				"To Verify as an user login into the Application, User will be able to copy a search query into the new search box by clicking on 'Copy to new search' in advanced search page");
+
+		loginPage.loginToSightLine(username, password);
+		
+		sessionSearch.navigateToAdvancedSearchPage();
+		sessionSearch.advancedContentSearchConfigure(Input.TallySearch);
+		sessionSearch.getAdvancedContentSearchInputAudio().Enter();
+		
+		sessionSearch.verifyQueryPresentinSearchbox("1", Input.TallySearch);
+		
+		baseClass.waitForElement(sessionSearch.getAdvSearchCopyToNewSearch());
+		sessionSearch.getAdvSearchCopyToNewSearch().Click(); 
+		sessionSearch.verifyQueryPresentinSearchbox("2", Input.TallySearch);
+		
+		
+		baseClass.passedStep("Verified - as an user login into the Application, "
+				+ "User will be able to copy a search query into the new search box by clicking on 'Copy to new search' in advanced search page");
+		loginPage.logout();
+
+	}
+	
+	@Test(description ="RPMXCON-57078", dataProvider = "Users", groups = { "regression" })
+	public void verifyRemoveFunctionallityAudioAdvSearch(String username, String password) throws InterruptedException {
+		BaseClass baseClass = new BaseClass(driver);
+		LoginPage loginPage = new LoginPage(driver);
+		SessionSearch sessionSearch = new SessionSearch(driver);
+		baseClass.stepInfo("Test case Id: RPMXCON-57078");
+		baseClass.stepInfo("Verify that Remove functionality working fine for 'Audio' block on 'Advanced Search' screen");
+		
+		loginPage.loginToSightLine(username, password);
+	
+		sessionSearch.navigateToSessionSearchPageURL();
+		sessionSearch.navigateToAdvancedSearchPage();	
+		
+		baseClass.waitForElement(sessionSearch.getAs_Audio());
+		sessionSearch.getAs_Audio().waitAndClick(3);
+		
+		if(sessionSearch.getAs_AudioLanguage().isElementAvailable(10)) {
+			baseClass.passedStep("Audio Button Clicked & Audio Block Successfully Added");
+		}else {
+			baseClass.failedStep("Audio Block Not Added");
+		}
+		
+		baseClass.waitForElement(sessionSearch.getRemoveLink());
+		sessionSearch.getRemoveLink().waitAndClick(3);
+		
+		if(sessionSearch.getAs_AudioLanguage().isElementAvailable(10)== false) {
+			baseClass.passedStep("Remove Functionality Worked Properly");
+			baseClass.passedStep("Audio Block Successfully Removed");		
+		}else {
+			baseClass.failedStep("Remove Functionality Not Worked Properly");
+		}
+		
+		baseClass.passedStep("Verified - that Remove functionality working fine for \"Audio\" block on \"Advanced Search\" screen");
+		loginPage.logout();
+	}
+	
+	@Test(description ="RPMXCON-57242", dataProvider = "Users", groups = { "regression" })
+	public void verifyConceptTileRtnMetadataInAdvSrcScreen (String username, String password) throws InterruptedException {
+		BaseClass baseClass = new BaseClass(driver);
+		LoginPage loginPage = new LoginPage(driver);
+		SessionSearch sessionSearch = new SessionSearch(driver);
+		baseClass.stepInfo("Test case Id: RPMXCON-57242");
+		baseClass.stepInfo("Verify that Conceptual tile return the result for Metatadata Search in Advanced Search Screen.");
+		
+		loginPage.loginToSightLine(username, password);
+		sessionSearch.advancedMetaDataSearch(Input.docFileExt, null, ".xls", null );
+		
+		int pureHit =Integer.parseInt(sessionSearch.verifyPureHitsCount());
+		SoftAssert asserts = new SoftAssert();
+		asserts.assertNotNull(pureHit, "Verified that Search result return documents which satisfied above configured query");
+		
+		int conceptPureHit = sessionSearch.runAndVerifyConceptualSearch();
+		
+		if(sessionSearch.getConceptualTileHit(conceptPureHit).Visible()) {
+			baseClass.passedStep("Verified that Conceptual tile return the result for Metadata Search in Advanced  Search Screen");
+		}else {
+			baseClass.failedStep("Still Loading on the Screen");
+		}
+		baseClass.passedStep("Verified - that Conceptual tile return the result for Metatadata Search in Advanced Search Screen");
+		loginPage.logout();
 	}
 
 	@BeforeMethod(alwaysRun = true)
