a46e29810cf6246ac9c66f5563fd2d6477b90ccc
Sowndarya.Velraj <Sowndarya.Velraj@LVS-W10-DEVS082.ConsilioTest.com>
2022-07-18 19:12:42 +0530

    committing Dashboard Component cases

--- sightline/src/main/java/testScriptsRegressionSprint17/Dashboard_Regression.java
+++ sightline/src/main/java/testScriptsRegressionSprint17/Dashboard_Regression.java
@@ -173,25 +173,144 @@ public class Dashboard_Regression {
 		if (dashBoard.mostCompletedDocs_InsideWidget().isDisplayed()) {
 			baseClass.passedStep("Widget displayed as per selected criteria");
 		}
-
 	}
-	
+
 	/**
 	 * @author Sowndarya.Velraj created on:NA modified by:NA TESTCASE
 	 *         No:RPMXCON-54207
-	 * @Description:To verify that RMU can view the details on Tag Count Widget by selecting favourite Tags.
+	 * @Description:To verify that RMU can view the details on Tag Count Widget by
+	 *                 selecting favourite Tags.
 	 **/
 	@Test(description = "RPMXCON-54207", enabled = true, groups = { "regression" })
 	public void verifyTaggingWidget() throws Exception {
 
 		baseClass.stepInfo("Test case Id:RPMXCON-54207 Dashboard Component Sprint 17");
-		baseClass.stepInfo(
-				"To verify that RMU can view the details on Tag Count Widget by selecting favourite Tags.");
+		baseClass.stepInfo("To verify that RMU can view the details on Tag Count Widget by selecting favourite Tags.");
 		UtilityLog.info(Input.prodPath);
 		Dashboard dashBoard = new Dashboard(driver);
 		dashBoard.AddNewWidgetToDashboard(Input.Tagging);
 		dashBoard.verifyPrivilegedAndResponsiveTags();
 		dashBoard.verifySelectedTagsInTaggingWidget();
-	
+
 	}
+
+	/**
+	 * @author Sowndarya.Velraj created on:NA modified by:NA TESTCASE
+	 *         No:RPMXCON-54212
+	 * @Description:To verify that details in End to End is displayed correctly
+	 **/
+	@Test(description = "RPMXCON-54212", enabled = true, groups = { "regression" })
+	public void verifyEyeToEyeWidget() throws Exception {
+
+		baseClass.stepInfo("Test case Id:RPMXCON-54212 Dashboard Component Sprint 17");
+		baseClass.stepInfo("To verify that details in End to End is displayed correctly.");
+		UtilityLog.info(Input.prodPath);
+		Dashboard dashBoard = new Dashboard(driver);
+		dashBoard.AddNewWidgetToDashboard(Input.EndtoEnd);
+		System.out.println("Released Count :" + dashBoard.releasedCount_EndToEnd().getText());
+		System.out.println("Not Reviewed Count :" + dashBoard.notReviewedCount_EndToEnd().getText());
+		System.out.println("Reviewed Count :" + dashBoard.reviewedCount_EndToEnd().getText());
+		System.out.println("Total produced Count :" + dashBoard.totalProducedCount().getText());
+
+		String value = dashBoard.testCS().GetCssValue("left");
+		System.out.println(value);
+
+
+	}
+
+	/**
+	 * @author Sowndarya.Velraj created on:NA modified by:NA TESTCASE
+	 *         No:RPMXCON-54876
+	 * @Description:Verify "My Assignment to Work" widget on RMU dashboard
+	 **/
+	@Test(description = "RPMXCON-54876", enabled = true, groups = { "regression" })
+	public void verifyMyAssignmentWidget() throws Exception {
+
+		baseClass.stepInfo("Test case Id:RPMXCON-54876 Dashboard Component Sprint 17");
+		baseClass.stepInfo("Verify My Assignment to Work widget on RMU dashboard");
+
+		UtilityLog.info(Input.prodPath);
+		SessionSearch sessionsearch = new SessionSearch(driver);
+		AssignmentsPage agnmt = new AssignmentsPage(driver);
+		Dashboard dashBoard = new Dashboard(driver);
+
+		String assignmentName = "Assignment" + Utility.dynamicNameAppender();
+		;
+
+		sessionsearch.basicContentSearch(Input.testData1);
+		sessionsearch.verifyPureHitsCount();
+		sessionsearch.bulkAssign();
+		agnmt.FinalizeAssignmentAfterBulkAssign();
+		agnmt.createAssignment_fromAssignUnassignPopup(assignmentName, Input.codeFormName);
+		agnmt.getAssignmentSaveButton().waitAndClick(5);
+		baseClass.stepInfo("Created a assignment " + assignmentName);
+		agnmt.editAssignmentUsingPaginationConcept(assignmentName);
+		driver.waitForPageToBeReady();
+		baseClass.stepInfo(assignmentName + " assignment opened in edit mode");
+		agnmt.assignmentDistributingToReviewerManager();
+
+		dashBoard.navigateToDashboard();
+		baseClass.waitForElement(dashBoard.getSelectAssignmentFromDashborad(assignmentName));
+		dashBoard.verifyAssignmentData(assignmentName);
+	}
+
+	/**
+	 * @author Sowndarya.Velraj created on:NA modified by:NA TESTCASE
+	 *         No:RPMXCON-54577
+	 * @Description:PAU and DAU users should be included for "Reviewer Progress"
+	 *                  widget on RMU Dashboard(Impersonate form SystemAdmin/PAU/DAU
+	 *                  to RMU)
+	 **/
+	@Test(description = "RPMXCON-54577", enabled = true, groups = { "regression" })
+	public void verifyReviewerProgressOnDashboard() throws Exception {
+
+		baseClass.stepInfo("Test case Id:RPMXCON-54577 Dashboard Component Sprint 17");
+		baseClass.stepInfo(
+				"PAU and DAU users should be included for Reviewer Progress widget on RMU Dashboard(Impersonate form SystemAdmin/PAU/DAU to RMU)");
+
+		UtilityLog.info(Input.prodPath);
+		SessionSearch sessionsearch = new SessionSearch(driver);
+		AssignmentsPage agnmt = new AssignmentsPage(driver);
+		DocViewPage docViewPage = new DocViewPage(driver);
+		Dashboard dashBoard = new Dashboard(driver);
+		String SaveSearchName = "NewSearch" + UtilityLog.dynamicNameAppender();
+		String assignmentName = "Assignment" + Utility.dynamicNameAppender();
+
+//		sessionsearch.basicContentSearch(Input.testData1);
+//		sessionsearch.verifyPureHitsCount();
+//		sessionsearch.bulkAssign();
+//		agnmt.FinalizeAssignmentAfterBulkAssign();
+//		agnmt.createAssignment_fromAssignUnassignPopup(assignmentName, Input.codeFormName);
+//		agnmt.getAssignmentSaveButton().waitAndClick(5);
+//		baseClass.stepInfo("Created a assignment " + assignmentName);
+//		agnmt.editAssignmentUsingPaginationConcept(assignmentName);
+//		driver.waitForPageToBeReady();
+//		baseClass.stepInfo(assignmentName + " assignment opened in edit mode");
+//		agnmt.add4ReviewerAndDistribute();
+
+		sessionsearch.basicContentSearch(Input.testData1);
+		baseClass.stepInfo("Search for text input completed");
+		String Hits = sessionsearch.verifyPureHitsCount();
+		sessionsearch.saveSearch(SaveSearchName);
+		sessionsearch.bulkAssign();
+		// create Assignment and disturbute docs
+		agnmt.assignmentCreation(assignmentName, Input.codeFormName);
+		agnmt.add4ReviewerAndDistribute();
+		baseClass.stepInfo(assignmentName + " Assignment Created and distributed to DA/PA/RMU/Rev");
+
+	}
+
+	@Test(description = "RPMXCON-54876", enabled = true, groups = { "regression" })
+	public void test() throws Exception {
+
+		baseClass.stepInfo("Test case Id:RPMXCON-54876 Dashboard Component Sprint 17");
+		baseClass.stepInfo("Verify My Assignment to Work widget on RMU dashboard");
+
+		UtilityLog.info(Input.prodPath);
+		Dashboard dashBoard = new Dashboard(driver);
+
+		dashBoard.testCS().GetAttribute("");
+
+	}
+
 }
