b8a386b7653b1e87327072a965c6f9039a64bd00
ArunkumarAadmin <ArunkumarAadmin@LVS-W10-DEVS070.ConsilioTest.com>
2022-08-09 15:33:29 +0530

    Committing Export Testcase RPMXCON-49135

---
+++ sightline/src/main/java/testScriptsRegressionSprint19/Export_Regression19.java
@@ -0,0 +1,146 @@
+package testScriptsRegressionSprint19;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.text.ParseException;
+
+import org.testng.ITestResult;
+import org.testng.Reporter;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import automationLibrary.Driver;
+import executionMaintenance.UtilityLog;
+import pageFactory.BaseClass;
+import pageFactory.DocViewPage;
+import pageFactory.LoginPage;
+import pageFactory.ProductionPage;
+import pageFactory.SessionSearch;
+import pageFactory.TagsAndFoldersPage;
+import pageFactory.Utility;
+import testScriptsSmoke.Input;
+
+public class Export_Regression19 {
+	Driver driver;
+	LoginPage loginPage;
+	BaseClass base;
+	DocViewPage docView;
+	Utility utility;
+
+	@BeforeClass(alwaysRun = true)
+	public void preConditions() throws InterruptedException, ParseException, IOException {
+		System.out.println("******Execution started for " + this.getClass().getSimpleName() + "********");
+		UtilityLog.info("******Execution started for " + this.getClass().getSimpleName() + "********");
+		UtilityLog.info("Started Execution for prerequisite");
+		Input input = new Input();
+		input.loadEnvConfig();
+
+	}
+
+	@BeforeMethod(alwaysRun = true)
+	public void beforeTestMethod(ITestResult result, Method testMethod) throws IOException {
+		Reporter.setCurrentTestResult(result);
+		System.out.println("------------------------------------------");
+		System.out.println("Executing method :  " + testMethod.getName());
+		UtilityLog.info(testMethod.getName());
+
+		driver = new Driver();
+		base = new BaseClass(driver);
+		loginPage = new LoginPage(driver);
+		loginPage.loginToSightLine(Input.pa1userName, Input.pa1password);
+		UtilityLog.info("Logged in as User: " + Input.pa1userName);
+		Reporter.log("Logged in as User: " + Input.pa1password);
+	}
+	
+
+	/**
+	 * Author :Arunkumar date: 09/08/2022 TestCase Id:RPMXCON-49135
+	 * Description :Verify in Production-Export, if default option is selected in text section, Privileged placeholder is enabled 
+	 * in TIFF/PDF component,and no text file exists in the SL Workspace then Priv placeholder text is exported
+	 */
+	@Test(description = "RPMXCON-49135", enabled = true, groups = { "regression" })
+	public void verifyPrivPlaceholderTextExported() throws Exception {
+		
+		base.stepInfo("Test case Id: RPMXCON-49135");
+		base.stepInfo("Verify Priv placeholder text is exported");
+
+		String foldername = "FolderProd" + Utility.dynamicNameAppender();
+		String tagname = "Tag" + Utility.dynamicNameAppender();
+		String newExport = "Export" + Utility.dynamicNameAppender();
+		String prefixID = Input.randomText + Utility.dynamicNameAppender();
+		String suffixID = Input.randomText + Utility.dynamicNameAppender();
+		String productionname = "p" + Utility.dynamicNameAppender();
+	
+		TagsAndFoldersPage tagsAndFolderPage = new TagsAndFoldersPage(driver);
+		tagsAndFolderPage.CreateFolder(foldername, Input.securityGroup);
+		tagsAndFolderPage.createNewTagwithClassification(tagname, Input.tagNamePrev);
+		SessionSearch sessionSearch = new SessionSearch(driver);
+		int purehit = sessionSearch.basicContentSearch(Input.telecaSearchString);
+		sessionSearch.bulkFolderExisting(foldername);
+		sessionSearch.bulkTagExisting(tagname);
+		sessionSearch.bulkRelease(Input.securityGroup);
+		ProductionPage page = new ProductionPage(driver);
+		String subBates = page.getRandomNumber(2);
+		page.selectingDefaultSecurityGroup();
+		String text = page.getProdExport_ProductionSets().getText();
+		if (text.contains("Export Set")) {
+			page.selectExportSetFromDropDown();
+		} else {
+			page.createNewExport(newExport);
+		}
+		page.addANewExport(productionname);
+		page.fillingDATSection();
+		page.selectPrivDocsInTiffSection(tagname);
+		base.stepInfo("Select text component and mark complete");
+		page.fillingTextSection();
+		page.navigateToNextSection();
+		page.fillingExportNumberingAndSortingPage(prefixID, suffixID, subBates);
+		page.navigateToNextSection();
+		page.fillingDocumentSelectionPage(foldername);
+		page.navigateToNextSection();
+		page.fillingPrivGuardPage();
+		page.fillingExportLocationPage(productionname);
+		page.navigateToNextSection();
+		page.fillingSummaryAndPreview();
+		base.stepInfo("generate production and verify");
+		page.fillingGeneratePageWithContinueGenerationPopupWithoutCommit();
+		page.getCopyPath().waitAndClick(2);
+		String actualCopedText = page.getCopiedTextFromClipBoard();
+		String parentTab = page.openNewTab(actualCopedText);
+		page.goToImageFiles();
+		page.verifyTiffFile(purehit, prefixID, suffixID, subBates, Input.tagNameTechnical);
+		driver.close();
+		driver.getWebDriver().switchTo().window(parentTab);
+		loginPage.logout();
+
+	}
+	
+	@AfterMethod(alwaysRun = true)
+	public void takeScreenShot(ITestResult result) {
+		if (ITestResult.FAILURE == result.getStatus()) {
+			Utility bc = new Utility(driver);
+			bc.screenShot(result);
+			System.out.println("Executed :" + result.getMethod().getMethodName());
+
+		}
+		try {
+			loginPage.quitBrowser();
+		} catch (Exception e) {
+			loginPage.quitBrowser();
+		}
+	}
+
+	@AfterClass(alwaysRun = true)
+	public void close() {
+		try {
+			// LoginPage.clearBrowserCache();
+
+		} catch (Exception e) {
+			System.out.println("Sessions already closed");
+		}
+	}
+
+}
