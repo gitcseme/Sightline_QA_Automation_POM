ddd2f249a09cbe27bcf5a8bdd2b92179e8b54603
Swathi.Ganta <Swathi.Ganta@LNT-HZ-NP-SQA08.consilio.com>
2022-09-09 09:17:34 -0500

    Added initiazition in before method

--- sightline/src/main/java/sightline/basicSearch/BasicSearch_Regression1.java
+++ sightline/src/main/java/sightline/basicSearch/BasicSearch_Regression1.java
@@ -47,8 +47,8 @@ public class BasicSearch_Regression1 {
 	public void preCondition() throws ParseException, InterruptedException, IOException {
 
 		System.out.println("******Execution started for " + this.getClass().getSimpleName() + "********");
-		Input in = new Input();
-    	in.loadEnvConfig();
+//		Input in = new Input();
+//    	in.loadEnvConfig();
 	}
 
 	@BeforeMethod
