3065d65838f297d6914cbe78f7cc012bca0becfe
smangal <smangal@LVS-W10-SQA001.consilio.com>
2022-09-15 16:07:27 +0530

    Ingestion fixes

--- sightline/src/main/java/pageFactory/IngestionPage_Indium.java
+++ sightline/src/main/java/pageFactory/IngestionPage_Indium.java
@@ -2041,6 +2041,7 @@ public class IngestionPage_Indium {
 			getSourceSelectionDATKey().ScrollTo();
 			base.waitForElement(getSourceSelectionDATKey());
 			getSourceSelectionDATKey().isElementAvailable(15);
+			base.waitTime(2);
 			getSourceSelectionDATKey().selectFromDropdown().selectByVisibleText(documentKey);
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -5938,7 +5939,7 @@ public class IngestionPage_Indium {
 				nextButton().isElementAvailable(10);
 				nextButton().waitAndClick(10);
 				if (disabledNextButton().isDisplayed()) {
-					base.waitTime(3);
+					base.waitTime(1);
 					unPunlishSearch(savedSearch).isElementAvailable(10);
 					if (unPunlishSearch(savedSearch).isDisplayed()) {
 						base.passedStep("Unpublish of '" + savedSearch + "' is in progess");
@@ -6770,7 +6771,7 @@ public class IngestionPage_Indium {
 		getFilterByINPROGRESS().waitAndClick(5);
 		base.waitForElement(getFilterByPUBLISHED());
 		getFilterByPUBLISHED().waitAndClick(5);
-
+		base.waitTime(3);
 		base.waitForElement(getIngestion_GridView());
 		getIngestion_GridView().waitAndClick(10);
 		base.waitTime(3);
@@ -6796,6 +6797,7 @@ public class IngestionPage_Indium {
 			ingestedCount = Integer.parseInt(totalDocsIngestedCount);
 			
 		}
+		getRefreshButton().waitAndClick(5);
 		if(ingestedCount==0) {
 			base.passedStep("No ingestion is currently present");
 			count =1;
@@ -6808,7 +6810,7 @@ public class IngestionPage_Indium {
 		}), Input.wait60);
 		String totalCount = getTotalPageCount().getText();
 		int pageCount = Integer.parseInt(getTotalPageCount().getText());
-		
+		getRefreshButton().waitAndClick(5);
 		if ((String.valueOf(totalCount)).contains("0")) {
 			count = Integer.parseInt(totalCount) / 10;
 		} else if (pageCount < 10) {
@@ -6818,7 +6820,7 @@ public class IngestionPage_Indium {
 		}
 		}
 		System.out.println(count);
-		
+		getRefreshButton().waitAndClick(5);
 		boolean status = false;
 		for (int i = 1; i <= count; i++) {
 
@@ -8280,8 +8282,9 @@ public class IngestionPage_Indium {
 		getFilterByPUBLISHED().waitAndClick(10);
 
 		getRefreshButton().waitAndClick(5);
-		base.waitTime(3);
+		base.waitTime(5);
 
+		base.waitForElement(getTotalIngestedCount());
 		String totalDocsIngestedCount = getTotalIngestedCount().getText();	
 		int ingestedCount;
 		
