18eddf3050b93702a9df42fa2d720f65aa41f7cd
Jeevitha.Rajendran <Jeevitha.Rajendran@LVS-W10-DEVS065.ConsilioTest.com>
2022-10-18 17:58:57 +0530

    Committing : RPMXCON-47830 , 53637,57280,56508,56595

---
+++ sightline/src/main/java/testScriptsRegressionSprint24/BatchPrintRegression_24.java
@@ -0,0 +1,170 @@
+package testScriptsRegressionSprint24;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.text.ParseException;
+import org.testng.ITestResult;
+import org.testng.Reporter;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import automationLibrary.Driver;
+import executionMaintenance.UtilityLog;
+import pageFactory.BaseClass;
+import pageFactory.BatchPrintPage;
+import pageFactory.LoginPage;
+import pageFactory.ProductionPage;
+import pageFactory.SessionSearch;
+import pageFactory.TagsAndFoldersPage;
+import pageFactory.Utility;
+import testScriptsSmoke.Input;
+
+public class BatchPrintRegression_24 {
+	Driver driver;
+	LoginPage loginPage;
+	BaseClass baseClass;
+	BatchPrintPage batchPrint;
+	SessionSearch session;
+	TagsAndFoldersPage tagsAndFolderPage;
+
+	@BeforeClass(alwaysRun = true)
+	public void preConditions() throws InterruptedException, ParseException, IOException {
+		System.out.println("******Execution started for " + this.getClass().getSimpleName() + "********");
+		UtilityLog.info("******Execution started for " + this.getClass().getSimpleName() + "********");
+		UtilityLog.info("Started Execution for prerequisite");
+
+		Input in = new Input();
+		in.loadEnvConfig();
+
+	}
+
+	@BeforeMethod(alwaysRun = true)
+	public void beforeTestMethod(ITestResult result, Method testMethod) throws IOException {
+		Reporter.setCurrentTestResult(result);
+		System.out.println("------------------------------------------");
+		System.out.println("Executing method :  " + testMethod.getName());
+		UtilityLog.info(testMethod.getName());
+
+		driver = new Driver();
+		loginPage = new LoginPage(driver);
+		baseClass = new BaseClass(driver);
+		batchPrint = new BatchPrintPage(driver);
+		session = new SessionSearch(driver);
+		tagsAndFolderPage = new TagsAndFoldersPage(driver);
+	}
+
+	@DataProvider(name = "Users")
+	public Object[][] Users() {
+		Object[][] users = { { Input.pa1userName, Input.pa1password }, { Input.rmu1userName, Input.rmu1password }, };
+		return users;
+	}
+
+	/**
+	 * @Author Jeevitha
+	 * @Description : To verify that user can use the prior production slip sheets
+	 *              for batch print.[RPMXCON-47830]
+	 * @throws Exception
+	 */
+	@Test(description = "RPMXCON-47830", enabled = true, groups = { "regression" })
+	public void verifyCanUsePrioProdForBatchPrint() throws Exception {
+		String prefixID = "A_" + Utility.dynamicNameAppender();
+		String suffixID = "_P" + Utility.dynamicNameAppender();
+		String Folder = "Folder" + Utility.dynamicNameAppender();
+		String slipsheetDD = "Create new slip sheets";
+
+		// Login As User
+		loginPage.loginToSightLine(Input.pa1userName, Input.pa1password);
+
+		baseClass.stepInfo("Test case Id: RPMXCON-47830 Batch Print");
+		baseClass.stepInfo("To verify that user can use the prior production slip sheets for batch print.");
+
+		// create folder in Default SG
+		tagsAndFolderPage.CreateFolder(Folder, Input.securityGroup);
+
+		// configure query & bulk folder
+		session.basicContentSearch(Input.testData1);
+		session.bulkFolder(Folder);
+
+		// Generate Production with dat,TIFF,native & select Slipsheet
+		String productionname = "P" + Utility.dynamicNameAppender();
+		ProductionPage page = new ProductionPage(driver);
+		String beginningBates = page.getRandomNumber(2);
+		System.out.println(productionname);
+		page.selectingDefaultSecurityGroup();
+		page.addANewProduction(productionname);
+		page.fillingDATSection();
+		page.fillingTIFFSectionwithBurnRedaction();
+		page.slipSheetToggleEnable();
+		page.getSlipCalculatedTabSelection().ScrollTo();
+		page.availableFieldSelection(Input.batesNumber);
+		page.navigateToNextSection();
+		page.InsertingDataFromNumberingToGenerateWithContinuePopup(prefixID, suffixID, Folder, productionname,
+				beginningBates);
+
+		// Select Folder & Production
+		batchPrint.navigateToBatchPrintPage();
+		batchPrint.fillingSourceSelectionTab("Folder", Folder, true);
+		batchPrint.fillingBasisForPrinting(false, true, productionname);
+		batchPrint.navigateToNextPage(1);
+		if (!batchPrint.getSlipSheetDD_prod().isElementAvailable(10)) {
+			batchPrint.navigateToNextPage(1);
+		}
+
+		// filling SlipSheet With metadata
+		batchPrint.selectDropdownFromSlipSheet_prod(slipsheetDD);
+		batchPrint.fillingSlipSheetWithMetadata(Input.documentKey, true, null);
+		if (!batchPrint.getSelectExportFileName().isElementAvailable(10)) {
+			batchPrint.navigateToNextPage(1);
+		}
+
+		// Filling Export File Name as 'Docfilename', select Sort by 'DOCID'
+		batchPrint.fillingExportFormatPage(Input.docFileName, Input.documentKey, false, 20);
+
+		// Download Batch Print File
+		String fileName = batchPrint.DownloadBatchPrintFile();
+
+		// extract zip file
+		String extractedFile = batchPrint.extractFile(fileName);
+
+		// verify Downloaded File Count ,filename and Format
+		batchPrint.verifyDownloadedFileCountAndFormat(Input.fileDownloadLocation + "\\" + extractedFile);
+
+		loginPage.logout();
+	}
+
+	@AfterMethod(alwaysRun = true)
+	public void takeScreenShot(ITestResult result, Method testMethod) {
+		Reporter.setCurrentTestResult(result);
+		UtilityLog.logafter(testMethod.getName());
+		if (ITestResult.FAILURE == result.getStatus()) {
+			Utility bc = new Utility(driver);
+			bc.screenShot(result);
+			try { // if any tc failed and dint logout!
+				loginPage.logoutWithoutAssert();
+			} catch (Exception e) {
+//		 								 TODO: handle exception
+			}
+		}
+		try {
+			loginPage.quitBrowser();
+		} catch (Exception e) {
+			loginPage.quitBrowser();
+			loginPage.clearBrowserCache();
+		}
+		System.out.println("Executed :" + result.getMethod().getMethodName());
+	}
+
+	@AfterClass(alwaysRun = true)
+	public void close() {
+
+		try {
+			loginPage.clearBrowserCache();
+		} finally {
+			loginPage.clearBrowserCache();
+		}
+	}
+}
