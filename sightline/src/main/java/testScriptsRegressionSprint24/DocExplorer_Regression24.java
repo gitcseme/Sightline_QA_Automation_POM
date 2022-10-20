1389c36fe2fdd77061290189c1aa4a4d7cba1378
Vijaya.M <Vijaya.M@LVS-W10-DEVS058.ConsilioTest.com>
2022-10-20 14:57:58 +0530

    Committing RPMXCON-54601,54715,54596,54728,54700 DocExplorer

--- sightline/src/main/java/testScriptsRegressionSprint24/DocExplorer_Regression24.java
+++ sightline/src/main/java/testScriptsRegressionSprint24/DocExplorer_Regression24.java
@@ -3,7 +3,11 @@ package testScriptsRegressionSprint24;
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.text.ParseException;
+import java.util.List;
 
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.ITestResult;
 import org.testng.Reporter;
 import org.testng.annotations.AfterClass;
@@ -14,10 +18,13 @@ import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 import automationLibrary.Driver;
+import automationLibrary.ElementCollection;
 import pageFactory.AssignmentsPage;
 import pageFactory.BaseClass;
+import pageFactory.CommentsPage;
 import pageFactory.DocExplorerPage;
 import pageFactory.DocListPage;
+import pageFactory.DomainDashboard;
 import pageFactory.LoginPage;
 import pageFactory.ReportsPage;
 import pageFactory.SavedSearch;
@@ -254,6 +261,226 @@ public class DocExplorer_Regression24 {
 		loginPage.logout();
 	}
 	
+	/**
+	 * Author :Vijaya.Rani date: 20/10/2022 TestCase Id:RPMXCON-54596 
+	 * Description:Verify when user selects multiple folders from the tree view
+	 * 
+	 * @throws Exception
+	 */
+	@Test(description = "RPMXCON-54596", enabled = true, groups = { "regression" })
+	public void verifySelectMultipleFoldersFormTreeView() throws Exception {
+
+		baseClass.stepInfo("Test case Id: RPMXCON-54596");
+		baseClass.stepInfo("Verify when user selects multiple folders from the tree view");
+
+		// Login As PA
+		loginPage.loginToSightLine(Input.pa1userName, Input.pa1password);
+		baseClass.stepInfo("User successfully logged into slightline webpage  PA as with " + Input.pa1userName + "");
+
+		//Select multiple folder and get count
+		ElementCollection Table = driver.FindElementsByXPath("//div[@id='divNodeTree']//ul//li");
+		List<WebElement> Tag = Table.FindWebElements();
+		
+		docExplorer.getfolderFromTreeByNumber("7").waitAndClick(5);
+		int fold = docExplorer.getcount("3");
+		Actions actions = new Actions(driver.getWebDriver());
+		actions.keyDown(Keys.LEFT_CONTROL).click(Tag.get(2));
+		int fold1 = docExplorer.getcount("2");
+		actions.click(Tag.get(3));
+		int fold2 = docExplorer.getcount("3");
+		actions.click(Tag.get(4));
+		int fold3 = docExplorer.getcount("4");
+		actions.keyUp(Keys.LEFT_CONTROL).build().perform();
+		int treeViewCount = fold+fold1 + fold2 + fold3;
+		System.out.println(treeViewCount);
+		baseClass.stepInfo("Docs count in multiple folder from treeview:" + treeViewCount);
+
+		int listViewCount = docExplorer.getDocumentCountFromListView();
+		baseClass.stepInfo("Docs count in rightside list view:" + listViewCount);
+		docExplorer.verifyDocsCountInFolderAndListView(treeViewCount, listViewCount);
+		loginPage.logout();
+	}
+
+	/**
+	 * Author :Vijaya.Rani date: 19/10/2022 TestCase Id:RPMXCON-54601
+	 * Description :Verify the Tree View for new project when Dataset is not uploaded and
+	 * Ingestion is not completed
+	 */
+	@Test(description = "RPMXCON-54601", enabled = true, groups = { "regression" })
+	public void verifyTreeViewNewProjectDatasetIsZero() throws Exception {
+
+		baseClass.stepInfo("Test case Id: RPMXCON-54601");
+		baseClass.stepInfo(
+				"Verify the Tree View for new project when Dataset is not uploaded and Ingestion is not completed");
+		SoftAssert softAssert = new SoftAssert();
+		// Login As PA
+		loginPage.loginToSightLine(Input.pa1userName, Input.pa1password);
+		baseClass.stepInfo("User successfully logged into slightline webpage  PA as with " + Input.pa1userName + "");
+		baseClass.selectproject(Input.NonDomainProject);
+
+		int expectedCount = 0;
+		int ActualCount = docExplorer.getAllDocumentFolderCountFromTreeView();
+		baseClass.stepInfo("Docs count in folder tree view:" + ActualCount);
+		softAssert.assertEquals(expectedCount, ActualCount);
+		baseClass.passedStep("Tree view is displayed with 'All Document' having count as zero ");
+		softAssert.assertAll();
+		loginPage.logout();
+	}
+
+	/**
+	 * Author :Vijaya.Rani date: 20/10/2022 TestCase Id:RPMXCON-54601 
+	 * Description:Verify the default menu when SA/DA impersonates as PA/RMU
+	 */
+	@Test(description = "RPMXCON-54601", enabled = true, groups = { "regression" })
+	public void verifyDefaultMenuSADAImpersonateAsPARMU() throws Exception {
+
+		baseClass.stepInfo("Test case Id: RPMXCON-54601");
+		baseClass.stepInfo("Verify the default menu when SA/DA impersonates as PA/RMU");
+		SoftAssert softAssert = new SoftAssert();
+		DomainDashboard domainDash = new DomainDashboard(driver);
+		DocExplorerPage docExplorer = new DocExplorerPage(driver);
+		// Login As SA
+		loginPage.loginToSightLine(Input.sa1userName, Input.sa1password);
+		baseClass.stepInfo("User successfully logged into slightline webpage  SA as with " + Input.sa1userName + "");
+		// impersonate SA to PA
+		baseClass.stepInfo("Impersonate As SA to PA");
+		baseClass.impersonateSAtoPA();
+		// verify docexplorer leftmenu panel
+		docExplorer.verifyDocExplorerInLeftMenu();
+		baseClass.stepInfo("Doc Explorer is presented in the left menu panel");
+		// check docexplorer above datasets
+		docExplorer.verifyDocExplorerAboveDatasets();
+		baseClass.stepInfo("Doc Explorer is present in the above datasets");
+		// docexplorerpage as default in PA
+		docExplorer.verifyDocExplorerPageHeader();
+		baseClass.stepInfo("After impersonating as PAU, \"Doc Explorer\" page is shown as default.");
+		loginPage.logout();
+
+		// Login As SA
+		loginPage.loginToSightLine(Input.sa1userName, Input.sa1password);
+		baseClass.stepInfo("User successfully logged into slightline webpage  SA as with " + Input.sa1userName + "");
+		// impersonate SA to RMU
+		baseClass.stepInfo("Impersonate As SA to RMU");
+		baseClass.impersonateSAtoRMU();
+		// verify docexplorer leftmenu panel
+		docExplorer.verifyDocExplorerInLeftMenu();
+		baseClass.stepInfo("Doc Explorer is presented in the left menu panel");
+		// check docexplorer after dashboard
+		docExplorer.verifyDocExplorerBelowDashBoard();
+		baseClass.stepInfo("Doc Explorer is present in the after dashBoard in RMU");
+		// DashBoard page as default in RMU
+		softAssert.assertTrue(domainDash.getUserHomePage().isDisplayed());
+		baseClass.passedStep("After impersonating as RMU, Dashboard page is shown by default.");
+		loginPage.logout();
+
+		// Login As DA
+		loginPage.loginToSightLine(Input.da1userName, Input.da1password);
+		baseClass.stepInfo("User successfully logged into slightline webpage  DA as with " + Input.da1userName + "");
+		// impersonate DA to PA
+		baseClass.stepInfo("Impersonate As DA to PA");
+		baseClass.impersonateDAtoPA();
+		// verify docexplorer leftmenu panel
+		docExplorer.verifyDocExplorerInLeftMenu();
+		baseClass.stepInfo("Doc Explorer is presented in the left menu panel");
+		// check docexplorer above datasets
+		docExplorer.verifyDocExplorerAboveDatasets();
+		baseClass.stepInfo("Doc Explorer is present in the above datasets");
+		// docexplorerpage as default in PA
+		docExplorer.verifyDocExplorerPageHeader();
+		baseClass.stepInfo("After impersonating as PAU, \"Doc Explorer\" page is shown as default.");
+		loginPage.logout();
+
+		// Login As DA
+		loginPage.loginToSightLine(Input.da1userName, Input.da1password);
+		baseClass.stepInfo("User successfully logged into slightline webpage  DA as with " + Input.da1userName + "");
+		// impersonate DA to RMU
+		baseClass.stepInfo("Impersonate As DA to RMU");
+		baseClass.impersonateDAtoRMU();
+		// verify docexplorer leftmenu panel
+		docExplorer.verifyDocExplorerInLeftMenu();
+		baseClass.stepInfo("Doc Explorer is presented in the left menu panel");
+		// check docexplorer after dashboard
+		docExplorer.verifyDocExplorerBelowDashBoard();
+		baseClass.stepInfo("Doc Explorer is present in the after dashBoard in RMU");
+		// DashBoard page as default in RMU
+		softAssert.assertTrue(domainDash.getUserHomePage().isDisplayed());
+		baseClass.passedStep("After impersonating as RMU, Dashboard page is shown by default.");
+		softAssert.assertAll();
+		loginPage.logout();
+	}
+
+	/**
+	 * @author Vijaya.Rani ModifyDate:20/10/2022 RPMXCON-54728                
+	 * @throws Exception
+	 * @Description Verify that “EmailSubject/Filename” Column header Filter with special characters is working correctly on Doc Explorer list.
+	 */
+	@Test(description = "RPMXCON-54728", enabled = true, groups = { "regression" })
+	public void verifyEmailSubjectFilenameColumnHeaderFilterWithSpecialChars() throws Exception {
+
+		baseClass.stepInfo("Test case Id: RPMXCON-54728");
+		baseClass.stepInfo(
+				"Verify that “EmailSubject/Filename” Column header Filter with special characters is working correctly on Doc Explorer list.");
+
+		DocExplorerPage docexp = new DocExplorerPage(driver);
+		String[] specialChars = {"~","!","@","#","$","%","&",";","(",")","_","-","+","=","[","]","?","/",".","'" };
+
+		// Login As PA
+		loginPage.loginToSightLine(Input.pa1userName, Input.pa1password);
+		baseClass.stepInfo("User successfully logged into slightline webpage  PA as with " + Input.pa1userName + "");
+
+		// verify EmailRecipient names in Special Chars
+		docexp.verifyEmailSubjectFilenameValuesInDocExp(specialChars);
+		baseClass.passedStep(
+				"Verify that “EmailSubject/Filename” Column header Filter with special characters is working Successfully on Doc Explorer list");
+
+		loginPage.logout();
+	}
+
+	/**
+	 * @author Vijaya.Rani ModifyDate:20/10/2022 RPMXCON-54700
+	 * @throws Exception
+	 * @Description Verify that “Comments” Filter with "Exclude" functionality is working correctly on Doc Explorer list.
+	 */
+	@Test(description = "RPMXCON-54700", enabled = true, groups = { "regression" })
+	public void verifyCommentsWithExcludeInDocExplorerList() throws Exception {
+
+		baseClass.stepInfo("Test case Id: RPMXCON-54700");
+		baseClass.stepInfo(
+				"Verify that “Comments” Filter with \"Exclude\" functionality is working correctly on Doc Explorer list.");
+
+		// Login As PA
+		loginPage.loginToSightLine(Input.pa1userName, Input.pa1password);
+		baseClass.stepInfo("User successfully logged into slightline webpage  PA as with " + Input.pa1userName + "");
+
+		DocExplorerPage docExplorer = new DocExplorerPage(driver);
+		CommentsPage comments=new CommentsPage(driver);
+		String random = Input.reviewed+ Utility.dynamicNameAppender();
+		String random1 = "Completed"+ Utility.dynamicNameAppender();
+		
+		comments.navigateToCommentsPage();
+		comments.AddComments(random);
+		comments.AddComments(random1);
+
+		docExplorer.navigateToDocExplorerPage();
+		String Docs = docExplorer.getDocExp_DocID().getText();
+		baseClass.stepInfo("Perform exclude filter by Comments");
+		docExplorer.performExculdeCommentsFilter(random,null);
+
+		baseClass.stepInfo("Verify documents after applying exclude functionality by Comments");
+		docExplorer.verifyExcludeFunctionlityForComments(Docs);
+
+		baseClass.stepInfo("Refresh page");
+		docExplorer.refreshPage();
+
+		baseClass.stepInfo("Perform exclude filter by Comments");
+		docExplorer.performExculdeCommentsFilter(random,random1);
+
+		baseClass.stepInfo("Verify documents after applying exclude functionality by Comments");
+		docExplorer.verifyExcludeFunctionlityForComments(Docs);
+
+		loginPage.logout();
+
+	}
 	@AfterMethod(alwaysRun = true)
 	public void takeScreenShot(ITestResult result) {
 		Reporter.setCurrentTestResult(result);
