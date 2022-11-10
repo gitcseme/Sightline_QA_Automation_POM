9df8f1c4761ce214e322f5d1ca81344d86188960
Jeevitha.Rajendran <Jeevitha.Rajendran@LVS-W10-DEVS065.ConsilioTest.com>
2022-11-10 18:08:09 +0530

    Committing : RPMXCON-47836,56706, 56814,47828

--- sightline/src/main/java/pageFactory/ABMReportPage.java
+++ sightline/src/main/java/pageFactory/ABMReportPage.java
@@ -6,6 +6,7 @@ import java.util.concurrent.Callable;
 
 import javax.management.ListenerNotFoundException;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 
@@ -132,42 +133,58 @@ public class ABMReportPage {
 	public ElementCollection getElements() {
 		return driver.FindElementsByXPath("//*[@class='a-menu']");
 	}
+
 	public Element getBG_NotificationPopUp() {
 		return driver.FindElementByXPath("//p[contains(text(),'Your report is taking a')]");
 	}
+
 	public Element getNoButton() {
 		return driver.FindElementById("btnNo");
 	}
+
 	public Element getBullHornIcon_CC() {
 		return driver.FindElementByXPath("//i[@class='fa fa-bullhorn']//following::b[contains(@class,'badge')]");
 	}
+
 	public Element getRedBullHornIcon_CC() {
 		return driver.FindElementByXPath("//i[@class='fa fa-bullhorn']//following::b[contains(@class,'bg-color-red')]");
-		}
+	}
+
 	public Element getBullHornIcon() {
 		return driver.FindElementByXPath("//i[@class='fa fa-bullhorn']");
 	}
+
 	public Element getBullHornIconNotification(int i) {
-		return driver.FindElementByXPath("(//a[contains(text(),'Your Background Report with Notification ')])["+i+"]");
+		return driver
+				.FindElementByXPath("(//a[contains(text(),'Your Background Report with Notification ')])[" + i + "]");
 	}
+
 	public ElementCollection gettableHeaders() {
 		return driver.FindElementsByXPath("//table[@id='dtAdvanceBatchMangeReport']/thead/tr/th");
 	}
+
 	public Element getActionButton() {
 		return driver.FindElementByXPath("//button[@id='tallyactionbtn']//span[@class='fa fa-chevron-down']");
 	}
-	public Element getCheckBox_table(String assignName,int i) {
-		return driver.FindElementByXPath("//table[@id='dtAdvanceBatchMangeReport']/tbody/tr/td[text()='"+assignName+"']/parent::tr/td["+i+"]/table//td/input");
+
+	public Element getCheckBox_table(String assignName, int i) {
+		return driver.FindElementByXPath("//table[@id='dtAdvanceBatchMangeReport']/tbody/tr/td[text()='" + assignName
+				+ "']/parent::tr/td[" + i + "]/table//td/input");
 	}
-	public Element getDocCount_table(String assignName,int i) {
-		return driver.FindElementByXPath("//table[@id='dtAdvanceBatchMangeReport']/tbody/tr/td[text()='"+assignName+"']/parent::tr/td["+i+"]/table//td/label");
+
+	public Element getDocCount_table(String assignName, int i) {
+		return driver.FindElementByXPath("//table[@id='dtAdvanceBatchMangeReport']/tbody/tr/td[text()='" + assignName
+				+ "']/parent::tr/td[" + i + "]/table//td/label");
 	}
+
 	public Element getDocSlectCheckBox() {
 		return driver.FindElementByXPath("//td[@class=' tableCell']/input");
 	}
+
 	public Element getActionBulkTag() {
 		return driver.FindElementByXPath("//a[text()='Bulk Tag']");
 	}
+
 	public Element getActionBulkAssign() {
 		return driver.FindElementByXPath("//a[text()='Bulk Assign']");
 	}
@@ -175,6 +192,31 @@ public class ABMReportPage {
 	public Element getActionBulkFolder() {
 		return driver.FindElementByXPath("//a[text()='Bulk Folder']");
 	}
+
+	// added by jeevitha
+
+	public Element getClickedAssignment_CB(String assignment) {
+		return driver.FindElementByXPath(
+				"//div[@id='jstreeAssignmentGroup']//a[@class='jstree-anchor jstree-clicked' and text()='" + assignment
+						+ "']");
+	}
+
+	public Element getTotalDocsSelected() {
+		return driver.FindElementByXPath("//label[text()='Total Net Selected Docs ']//span");
+	}
+
+	public Element getABM_ReportPage() {
+		return driver.FindElementById("dvAdvancedBatchreport");
+	}
+
+	public Element getABM_Assignsavebutton() {
+		return driver.FindElementById("assignment");
+	}
+
+	public Element getSourceAssignment(String assignment) {
+		return driver.FindElementByXPath("//div[@id='assignmentsJSTree']//a[text()='" + assignment + "']");
+	}
+
 	public ABMReportPage(Driver driver) {
 
 		this.driver = driver;
@@ -304,8 +346,8 @@ public class ABMReportPage {
 	}
 
 	/**
-	 * @author Jayanthi.ganesan
-	 *         This method will redirect from ABM Page to Doc view page.
+	 * @author Jayanthi.ganesan This method will redirect from ABM Page to Doc view
+	 *         page.
 	 */
 	public void ViewInDocView() {
 		driver.scrollPageToTop();
@@ -321,12 +363,14 @@ public class ABMReportPage {
 
 	/**
 	 * @author Jayanthi.ganesan
-	 * @param savedsearch (Name of saved search to be selected as source)
-	 * @param assgnname (Name of Assignment to be selected to generate report)
+	 * @param savedsearch            (Name of saved search to be selected as source)
+	 * @param assgnname              (Name of Assignment to be selected to generate
+	 *                               report)
 	 * @param manageBatch('true'--if ABM Report needs to generated at doc
-	 *                   Level(Manage Batch option selected as doc level) 
-	 *                   ('false'--if ABM Report needs to be generated at assignment 
-	 *                   Level(Manage Batch option selected as Assignment level)
+	 *                               Level(Manage Batch option selected as doc
+	 *                               level) ('false'--if ABM Report needs to be
+	 *                               generated at assignment Level(Manage Batch
+	 *                               option selected as Assignment level)
 	 * @return To Do Value for a particular assignment from ABM Report Generated .
 	 * @throws InterruptedException
 	 */
@@ -397,75 +441,70 @@ public class ABMReportPage {
 	}
 
 	/**
-	 * @author Jayanthi.ganesan
-	 *         This method will generate ABM Report and validate the Reviewer
-	 *         list in Report Page
+	 * @author Jayanthi.ganesan This method will generate ABM Report and validate
+	 *         the Reviewer list in Report Page
 	 * @param savedsearch(Name of saved search to be selected as source)
-	 * @param assgnname (Name of Assignment to be selected to generate report)
-	 * @param manageBatch (should be true if ABM needs to be generated at doc level)
+	 * @param assgnname        (Name of Assignment to be selected to generate
+	 *                         report)
+	 * @param manageBatch      (should be true if ABM needs to be generated at doc
+	 *                         level)
 	 * @throws InterruptedException
 	 */
 	public void validateRevListAndgenerateABM_Report(final String savedsearch, final String assgnname,
-            boolean manageBatch, boolean TocheckreviewerList) throws InterruptedException {
+			boolean manageBatch, boolean TocheckreviewerList) throws InterruptedException {
 
+		driver.getWebDriver().get(Input.url + "Report/ReportsLanding");
 
+		bc.waitForElement(getReport_ABM());
+		getReport_ABM().Click();
+		driver.waitForPageToBeReady();
+		if (manageBatch) {
+			getManageBatchAt().selectFromDropdown().selectByVisibleText("Document");
+		}
+		bc.waitForElement(getABM_SelectSource());
+		getABM_SelectSource().Click();
+		getABM_SearchButton().waitAndClick(10);
+		bc.waitForElement(getABM_SelectSearch(savedsearch));
+		getABM_SelectSearch(savedsearch).Click();
+		bc.waitForElement(getABM_Searchsavebutton());
+		getABM_Searchsavebutton().Click();
+		bc.waitForElement(getABM_ReviewerExpandbutton());
+		getABM_ReviewerExpandbutton().Click();
+		bc.waitForElement(getABM_Reviewer_SelectAll());
+		getABM_Reviewer_SelectAll().Click();
+		if (TocheckreviewerList) {
+			getABM_RevList(Input.pa1FullName).isElementAvailable(2);
+			// getABM_RevList(Input.da1FullName).isElementAvailable(2);
+			getABM_RevList(Input.rev1FullName).isElementAvailable(2);
+			getABM_RevList(Input.rmu1FullName).isElementAvailable(2);
+			bc.stepInfo("PA/DA/RMU/REviewer Associated to the project is avialable under ABM.");
+		}
+		getABM_ReviewerExpandbutton().ScrollTo();
+		getABM_ReviewerExpandbutton().Click();
+		bc.waitTime(3);
+		getABM_SelectAssignment().ScrollTo();
+		bc.waitTillElemetToBeClickable(getABM_SelectAssignment());
+		getABM_SelectAssignment().waitAndClick(20);
+		bc.waitTime(2);
+		getABM_SelectAssgn(assgnname).Click();
+		driver.scrollPageToTop();
+		getApplyBtn().waitAndClick(10);
+		driver.waitForPageToBeReady();
+		bc.waitForElement(getABM_SummaryPage());
+		if (getABM_SummaryPage().isDisplayed()) {
+			bc.passedStep("Advanced Batch Manage Report generated with saved search " + savedsearch
+					+ " as Source and assignment seelcted is " + assgnname);
+		} else {
+			bc.failedMessage("Advanced Batch Management report not generated.");
+		}
 
-       driver.getWebDriver().get(Input.url + "Report/ReportsLanding");
-
-
-
-       bc.waitForElement(getReport_ABM());
-        getReport_ABM().Click();
-        driver.waitForPageToBeReady();
-        if (manageBatch) {
-            getManageBatchAt().selectFromDropdown().selectByVisibleText("Document");
-        }
-        bc.waitForElement(getABM_SelectSource());
-        getABM_SelectSource().Click();
-        getABM_SearchButton().waitAndClick(10);
-        bc.waitForElement(getABM_SelectSearch(savedsearch));
-        getABM_SelectSearch(savedsearch).Click();
-        bc.waitForElement(getABM_Searchsavebutton());
-        getABM_Searchsavebutton().Click();
-        bc.waitForElement(getABM_ReviewerExpandbutton());
-        getABM_ReviewerExpandbutton().Click();
-        bc.waitForElement(getABM_Reviewer_SelectAll());
-        getABM_Reviewer_SelectAll().Click();
-        if (TocheckreviewerList) {
-            getABM_RevList(Input.pa1FullName).isElementAvailable(2);
-            // getABM_RevList(Input.da1FullName).isElementAvailable(2);
-            getABM_RevList(Input.rev1FullName).isElementAvailable(2);
-            getABM_RevList(Input.rmu1FullName).isElementAvailable(2);
-            bc.stepInfo("PA/DA/RMU/REviewer Associated to the project is avialable under ABM.");
-        }
-        getABM_ReviewerExpandbutton().ScrollTo();
-        getABM_ReviewerExpandbutton().Click();
-        bc.waitTime(3);
-        getABM_SelectAssignment().ScrollTo();
-        bc.waitTillElemetToBeClickable(getABM_SelectAssignment());
-        getABM_SelectAssignment().waitAndClick(20);
-        bc.waitTime(2);
-        getABM_SelectAssgn(assgnname).Click();
-        driver.scrollPageToTop();
-        getApplyBtn().waitAndClick(10);
-        driver.waitForPageToBeReady();
-        bc.waitForElement(getABM_SummaryPage());
-        if (getABM_SummaryPage().isDisplayed()) {
-            bc.passedStep("Advanced Batch Manage Report generated with saved search " + savedsearch
-                    + " as Source and assignment seelcted is " + assgnname);
-        } else {
-            bc.failedMessage("Advanced Batch Management report not generated.");
-        }
-
-
-
-   }
+	}
 
 	/**
 	 * @author Jayanthi.ganesan
 	 * @param elecollection
-	 * @Description This method will get the doc counts of one column (Eg-To
-	 *              Do Count ,Completed Doc Count ,etc)from report generated
+	 * @Description This method will get the doc counts of one column (Eg-To Do
+	 *              Count ,Completed Doc Count ,etc)from report generated
 	 * @return Sum of all values in that particular column.
 	 */
 	public int validateReport(ElementCollection elecollection) {
@@ -493,12 +532,12 @@ public class ABMReportPage {
 	 *         back ground notification
 	 * @param savedsearch
 	 * @param savedsearch_docLevel[this saved search should be having docs upto
-	 *                             10000 only because we have to use this
-	 *                             search to generate report at doc level ]
+	 *                                  10000 only because we have to use this
+	 *                                  search to generate report at doc level ]
 	 * @param assgnname
 	 * @param assignmentName1
-	 * @param manageBatch[if manage batch option is document level this
-	 *                    should be 'true']
+	 * @param manageBatch[if            manage batch option is document level this
+	 *                                  should be 'true']
 	 * @throws InterruptedException
 	 */
 	public void generateABM_BackGroundReportGeneration(String savedsearch, String savedsearch_docLevel,
@@ -576,6 +615,7 @@ public class ABMReportPage {
 			bc.failedMessage("No new notification added in bull horn icon");
 		}
 	}
+
 	/**
 	 * @author Jayanthi.ganesan
 	 * @param IndexElement
@@ -586,63 +626,166 @@ public class ABMReportPage {
 		i = bc.getIndex(gettableHeaders(), IndexElement);
 		return i;
 	}
+
 	/**
-	 * Select docs from Any column[In set,To do,Completed] as per our wish 
-	 * if we select Assignment ABM report. 
+	 * Select docs from Any column[In set,To do,Completed] as per our wish if we
+	 * select Assignment ABM report.
+	 * 
 	 * @param assignName
 	 * @param columnName
 	 * @param clickCheckBox
 	 * @return
 	 */
-	
-	public String selectDocsinTable(String assignName,String columnName,boolean clickCheckBox) {
+
+	public String selectDocsinTable(String assignName, String columnName, boolean clickCheckBox) {
 		driver.waitForPageToBeReady();
 		bc.waitForElementCollection(gettableHeaders());
-		int i=getIndex(columnName);
-		if(clickCheckBox) {
-			getCheckBox_table(assignName,i).ScrollTo();
-			getCheckBox_table(assignName,i).Click();
+		int i = getIndex(columnName);
+		if (clickCheckBox) {
+			getCheckBox_table(assignName, i).ScrollTo();
+			getCheckBox_table(assignName, i).Click();
 		}
-		System.out.println(getDocCount_table(assignName,i).getText());
-		return getDocCount_table(assignName,i).getText();
-		
+		System.out.println(getDocCount_table(assignName, i).getText());
+		return getDocCount_table(assignName, i).getText();
+
 	}
-	
-	
+
 	/**
 	 * doc selection if we select ABM report at document level
 	 */
 	public void docSelection() {
 		bc.waitForElement(getDocSlectCheckBox());
 		getDocSlectCheckBox().Click();
-		
+
 	}
+
 	/**
 	 * @author Jayanthi.ganesan
-	 * @param folder[if we want to do bulk folder it should 
-	 *  be true else bulk tag will be done]
+	 * @param folder[if we want to do bulk folder it should be true else bulk tag
+	 *                  will be done]
 	 * 
 	 */
-	public  void BulkTag_Folder(boolean folder)  {
+	public void BulkTag_Folder(boolean folder) {
 
 		bc.waitForElement(getActionButton());
 		getActionButton().waitAndClick(20);
-		if(folder) {
-		bc.waitForElement(getActionBulkFolder());
-		getActionBulkFolder().Click();
-		}else {
-		bc.waitForElement(getActionBulkTag());
-		getActionBulkTag().Click();
+		if (folder) {
+			bc.waitForElement(getActionBulkFolder());
+			getActionBulkFolder().Click();
+		} else {
+			bc.waitForElement(getActionBulkTag());
+			getActionBulkTag().Click();
 		}
 	}
+
 	/**
-	 * @author Jayanthi.ganesan 
+	 * @author Jayanthi.ganesan
 	 * 
 	 */
-	public  void bulkAssign()  {
+	public void bulkAssign() {
 		bc.waitForElement(getActionButton());
 		getActionButton().waitAndClick(20);
 		bc.waitForElement(getActionBulkAssign());
 		getActionBulkAssign().Click();
+	}
+
+	/**
+	 * @Author Jeevitha
+	 * @Dsecription : Select Sources from source selection tab
+	 * @param SelectionLink
+	 * @param Name
+	 */
+	public void selectSources(String SelectionLink, String Name) {
+		bc.waitForElement(getABM_SelectSource());
+		getABM_SelectSource().waitAndClick(5);
+
+		bc.waitForElement(getABM_AssignmentButton());
+		if (SelectionLink.equalsIgnoreCase("Assignments")) {
+			getABM_AssignmentButton().waitAndClick(5);
+			bc.waitForElement(getABM_AssignmentButton());
+			getSourceAssignment(Name).waitAndClick(5);
+			bc.waitForElement(getABM_AssignmentButton());
+			getABM_Assignsavebutton().waitAndClick(5);
+
+			bc.stepInfo("Selected Assignments in source slection tab is: " + Name);
+		}
+	}
+
+	/**
+	 * @Author Jeevitha
+	 * @Dsecription : Select Reviewers from Reviewer selection tab
+	 */
+	public void selectReviewers(boolean SelectAll, boolean closeReviewerExpand, String additional) {
+		bc.waitForElement(getABM_ReviewerExpandbutton());
+		getABM_ReviewerExpandbutton().waitAndClick(5);
+
+		if (SelectAll) {
+			bc.waitForElement(getABM_Reviewer_SelectAll());
+			getABM_Reviewer_SelectAll().waitAndClick(10);
+			bc.stepInfo("Selected All reviewers from Reviewers Tab");
+		}
+		if (closeReviewerExpand) {
+			getABM_ReviewerExpandbutton().ScrollTo();
+			bc.waitForElement(getABM_ReviewerExpandbutton());
+			getABM_ReviewerExpandbutton().waitAndClick(5);
+		}
+	}
+
+	/**
+	 * @Author Jeevitha
+	 * @Dsecription : Select Assignment from Assignment selection tab
+	 */
+	public void selectAssignment(boolean oneAssignment, String assgnname, String addition) {
+		driver.scrollingToBottomofAPage();
+		bc.waitForElement(getABM_SelectAssignment());
+		getABM_SelectAssignment().waitAndClick(10);
+		bc.waitTime(3);
+		if (oneAssignment) {
+			bc.waitForElement(getABM_SelectAssgn(assgnname));
+			getABM_SelectAssgn(assgnname).ScrollTo();
+			getABM_SelectAssgn(assgnname).waitAndClick(10);
+			bc.stepInfo("Selected Assignment From Assignment selection TAb is: " + assgnname);
+		}
+	}
+
+	/**
+	 * @Author Jeevitha
+	 * @Description : verify ABM generated Report
+	 * @param verifyDoc
+	 * @param selectedDocCount
+	 */
+	public void verifyReportGenerated(boolean clickApplyBtn, boolean verifyDoc, int selectedDocCount,
+			boolean verifyImpactOfSelectCriteria, String assignName) {
+		if (clickApplyBtn) {
+			driver.scrollPageToTop();
+			bc.waitForElement(getApplyBtn());
+			getApplyBtn().waitAndClick(10);
+			bc.stepInfo("Clicked Apply Button");
+		}
+
+		if (getABM_ReportPage().isElementAvailable(60)) {
+			bc.passedStep("Advanced Batch Management Report Generated Successfully");
+			bc.waitTime(3);
+			if (verifyDoc) {
+				bc.waitForElement(getTotalDocsSelected());
+				String actualCount = getTotalDocsSelected().getText();
+				bc.stepInfo("Total Net Selected Docs Displayed in Report is : " + actualCount);
+				bc.digitCompareEquals(Integer.parseInt(actualCount), selectedDocCount,
+						"Generated report is as per the selected criteria.",
+						"Generated report is Not as per the selected criteria.");
+			}
+
+			if (verifyImpactOfSelectCriteria) {
+				driver.waitForPageToBeReady();
+				bc.ValidateElement_Presence(getClickedAssignment_CB(assignName), "Selected Assignment");
+				driver.waitForPageToBeReady();
+				bc.printResutInReport(
+						driver.verifyElementPresence("return document.querySelector(\"#checkAll4\").checked"),
+						"Selected Reviewers is Retained", "Selected Reviewers is not retained", "Pass");
+				bc.stepInfo("Selected Reviewer and Assignment selection is not impacted after performing action.");
+			}
+		} else {
+			bc.failedStep("Advanced Batch Management Report is not Displayed");
+		}
 	}
 }
