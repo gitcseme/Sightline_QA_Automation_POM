647b0d95616a7d7ab418c94f783a2cdb27c2c2c8
Sai.Duvvuru <Sai.Duvvuru@LVS-W10-DEVS092.ConsilioTest.com>
2022-11-17 12:51:36 +0530

    comitting DocView fixes

--- sightline/src/main/java/testScriptsRegressionSprint25/DocView_Regression25.java
+++ sightline/src/main/java/testScriptsRegressionSprint25/DocView_Regression25.java
@@ -88,78 +88,76 @@ public class DocView_Regression25 {
 	 * 
 	 */
 	@Test(description ="RPMXCON-63806",enabled = true, alwaysRun = true, groups = { "regression" })
-	public void verifyTextRedacDocPerformCopyPasteInCodingForm() throws Exception {
-		baseClass = new BaseClass(driver);
-		baseClass.stepInfo("Test case Id: RPMXCON-63806");
-		baseClass.stepInfo("Verify user can select the text from the redacted document\n"
-				+ "		 * and perform Copy -Paste by using \"Ctrl C\" from viewer file and \"Ctrl V\" in to\n"
-				+ "		 * coding form field Verify user can select the text from the redacted document and perform Copy -Paste by using \"Ctrl C\" from viewer file and \"Ctrl V\" in to coding form field");
-		DocViewPage docView = new DocViewPage(driver);
-		DocViewRedactions docViewRedact = new DocViewRedactions(driver);
-		String docid = "ID00000152";
-		SoftAssert softassert = new SoftAssert();
-
-		// login as RMU
-		loginPage.loginToSightLine(Input.rmu1userName, Input.rmu1password);
-		baseClass.stepInfo("Login as Rmu");
-		docexp = new DocExplorerPage(driver);
-		DocListPage doclist = new DocListPage(driver);
-
-		// DocExploer to viewindocView Page
-		baseClass.stepInfo("DocExplorer Navigate To ViewInDocView");
-		this.driver.getWebDriver().get(Input.url + "DocExplorer/Explorer");
-		baseClass.waitForElement(docexp.getDocExp_DocFiletypeSearchName());
-		docexp.getDocExp_DocFiletypeSearchName().SendKeys("Text");
-		doclist.getApplyFilter().waitAndClick(10);
-		docexp.getDocExp_SelectAllDocs().isElementAvailable(10);
-		docexp.getDocExp_SelectAllDocs().Click();
-		driver.waitForPageToBeReady();
+    public void verifyTextRedacDocPerformCopyPasteInCodingForm() throws Exception {
+        baseClass = new BaseClass(driver);
+        baseClass.stepInfo("Test case Id: RPMXCON-63806");
+        baseClass.stepInfo("Verify user can select the text from the redacted document\n"
+                + "      * and perform Copy -Paste by using \"Ctrl C\" from viewer file and \"Ctrl V\" in to\n"
+                + "      * coding form field Verify user can select the text from the redacted document and perform Copy -Paste by using \"Ctrl C\" from viewer file and \"Ctrl V\" in to coding form field");
+        DocViewPage docView = new DocViewPage(driver);
+        DocViewRedactions docViewRedact = new DocViewRedactions(driver);
+        String docid = "ID00000152";
+        // login as RMU
+        loginPage.loginToSightLine(Input.rmu1userName, Input.rmu1password);
+        baseClass.stepInfo("Login as Rmu");
+        docexp = new DocExplorerPage(driver);
+        DocListPage doclist = new DocListPage(driver);
+        // DocExploer to viewindocView Page
+        baseClass.stepInfo("DocExplorer Navigate To ViewInDocView");
+        this.driver.getWebDriver().get(Input.url + "DocExplorer/Explorer");
+        baseClass.waitForElement(docexp.getDocExp_DocFiletypeSearchName());
+        docexp.getDocExp_DocFiletypeSearchName().SendKeys("Text");
+        doclist.getApplyFilter().waitAndClick(10);
+        docexp.getDocExp_SelectAllDocs().isElementAvailable(10);
+        docexp.getDocExp_SelectAllDocs().Click();
+        driver.waitForPageToBeReady();
         if (doclist.getYesAllPageDocs().isDisplayed()) {
             doclist.getYesAllPageDocs().waitAndClick(5);
             doclist.getPopUpOkBtn().waitAndClick(10);           
         }
         baseClass.waitTime(5);
-		docexp.docExpViewInDocView();
-		driver.waitForPageToBeReady();
-		docView.selectSourceDocIdInAvailableField("SourceDocID");
-		driver.Navigate().refresh();
-		docView.ScrollAndSelectDocument(docid);
-		docView.getDocView_CodingFormlist().waitAndClick(5);
-		docView.getDocView_CodingFormlist().selectFromDropdown().selectByVisibleText("Default Project Coding Form");
-		docViewRedact.redactRectangleUsingOffset(0, 0, 50, 100);
-		baseClass.waitForElement(docViewRedact.redactionSave());
-		docViewRedact.redactionSave().isElementAvailable(15);
-		docViewRedact.redactionSave().waitAndClick(5);
-		baseClass.VerifySuccessMessage("Redaction tags saved successfully.");
-
-		docView.verifyCopyAndPasteRedacTextOnCommentBox();
-		driver.waitForPageToBeReady();
-		docView.getCodingFormSaveThisForm().waitAndClick(2);
-		baseClass.stepInfo("Document saved successfully with  comments");
-		String beforeText = docView.getAddComment1().getText();
-		driver.waitForPageToBeReady();
-		docView.getCodeSameAsLast().waitAndClick(5);
-		baseClass.stepInfo("Clicked codesameas last Document saved successfully with comments");
-		docView.ScrollAndSelectDocument(docid);
-		driver.waitForPageToBeReady();
-		String AfterText = docView.getAddComment1().getText();
-		softassert.assertEquals(beforeText,AfterText);
-		baseClass.stepInfo("Document saved successfully with latest comments");
-		driver.Navigate().refresh();
-		driver.waitForPageToBeReady();
-		docView.ScrollAndSelectDocument(docid);
-		docView.verifyCopyAndPasteRedacTextOnCommentBoxOffSet(0, 0, 40, 100);
-		baseClass.stepInfo("Copied redacted text and paste into the Comments box");
-		driver.waitForPageToBeReady();
-		docView.getCodingFormSaveThisForm().waitAndClick(2);
-		baseClass.passedStep("Document saved successfully");
-
-		baseClass.stepInfo("Text and this page redaction as expected");
-		docViewRedact.redactRectangleUsingOffset(0, 0, 50, 100);
-		baseClass.waitForElement(docViewRedact.redactionSave());
-		docViewRedact.redactionSave().isElementAvailable(15);
-		docViewRedact.redactionSave().waitAndClick(5);
-		baseClass.VerifySuccessMessage("Redaction tags saved successfully.");
-		softassert.assertAll();
-	}
+        docexp.docExpViewInDocView();
+        driver.waitForPageToBeReady();
+        docView.selectSourceDocIdInAvailableField("SourceDocID");
+        driver.Navigate().refresh();
+        docView.ScrollAndSelectDocument(docid);
+        docView.getDocView_CodingFormlist().waitAndClick(5);
+        docView.getDocView_CodingFormlist().selectFromDropdown().selectByVisibleText("Default Project Coding Form");
+        docViewRedact.redactRectangleUsingOffset(0, 0, 50, 100);
+        baseClass.waitForElement(docViewRedact.redactionSave());
+        docViewRedact.redactionSave().isElementAvailable(15);
+        docViewRedact.redactionSave().waitAndClick(5);
+        baseClass.VerifySuccessMessage("Redaction tags saved successfully.");
+        docView.verifyCopyAndPasteRedacTextOnCommentBox();
+        driver.waitForPageToBeReady();
+        docView.getCodingFormSaveThisForm().waitAndClick(2);
+        baseClass.stepInfo("Document saved successfully with  comments");
+        String beforeText = docView.getAddComment1().getText();
+        driver.waitForPageToBeReady();
+        docView.getCodeSameAsLast().waitAndClick(5);
+        baseClass.stepInfo("Clicked codesameas last Document saved successfully with comments");
+        docView.ScrollAndSelectDocument(docid);
+        driver.waitForPageToBeReady();
+        String AfterText = docView.getAddComment1().getText();
+        if(beforeText.contains(AfterText)) {
+            baseClass.passedStep("The copied text is reflected as expected");
+        }else {
+            baseClass.failedStep("The copied text is reflected as expected");   
+        }
+        baseClass.stepInfo("Document saved successfully with latest comments");
+        driver.Navigate().refresh();
+        driver.waitForPageToBeReady();
+        docView.ScrollAndSelectDocument(docid);
+        docView.verifyCopyAndPasteRedacTextOnCommentBoxOffSet(0, 0, 40, 100);
+        baseClass.stepInfo("Copied redacted text and paste into the Comments box");
+        driver.waitForPageToBeReady();
+        docView.getCodingFormSaveThisForm().waitAndClick(2);
+        baseClass.passedStep("Document saved successfully");
+        baseClass.stepInfo("Text and this page redaction as expected");
+        docViewRedact.redactRectangleUsingOffset(0, 0, 50, 100);
+        baseClass.waitForElement(docViewRedact.redactionSave());
+        docViewRedact.redactionSave().isElementAvailable(15);
+        docViewRedact.redactionSave().waitAndClick(5);
+        baseClass.VerifySuccessMessage("Redaction tags saved successfully.");
+    }
 }
