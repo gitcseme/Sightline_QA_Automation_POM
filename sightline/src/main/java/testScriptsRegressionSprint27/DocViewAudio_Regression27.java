97318860060ea45bc561754554939f5e67c0450d
Vijaya.M <Vijaya.M@LVS-W10-DEVS058.ConsilioTest.com>
2022-12-08 17:41:43 +0530

    Committing testCase DocViewAudio

---
+++ sightline/src/main/java/testScriptsRegressionSprint27/DocViewAudio_Regression27.java
@@ -0,0 +1,477 @@
+package testScriptsRegressionSprint27;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.text.ParseException;
+
+import org.testng.ITestResult;
+import org.testng.Reporter;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import automationLibrary.Driver;
+import pageFactory.AssignmentsPage;
+import pageFactory.BaseClass;
+import pageFactory.DocExplorerPage;
+import pageFactory.DocListPage;
+import pageFactory.DocViewPage;
+import pageFactory.LoginPage;
+import pageFactory.SavedSearch;
+import pageFactory.SessionSearch;
+import pageFactory.Utility;
+import testScriptsSmoke.Input;
+
+public class DocViewAudio_Regression27 {
+
+	Driver driver;
+	LoginPage loginPage;
+	SessionSearch sessionSearch;
+	SavedSearch savedSearch;
+	AssignmentsPage assignment;
+	BaseClass baseClass;
+	Input in;
+	SoftAssert softAssert;
+	DocExplorerPage docExplorer;
+	DocListPage docList;
+
+	@BeforeClass(alwaysRun = true)
+	public void preCondition() throws ParseException, InterruptedException, IOException {
+
+		in = new Input();
+		in.loadEnvConfig();
+		System.out.println("******Execution started for " + this.getClass().getSimpleName() + "********");
+
+	}
+
+	@BeforeMethod
+	public void beforeTestMethod(Method testMethod) {
+		System.out.println("------------------------------------------");
+		System.out.println("Executing method : " + testMethod.getName());
+		driver = new Driver();
+		assignment = new AssignmentsPage(driver);
+		loginPage = new LoginPage(driver);
+		baseClass = new BaseClass(driver);
+		sessionSearch = new SessionSearch(driver);
+		softAssert = new SoftAssert();
+		docExplorer = new DocExplorerPage(driver);
+
+	}
+	
+	
+	/**
+	 * @author Vijaya.Rani ModifyDate:08/12/2022 RPMXCON-51691
+	 * @throws InterruptedException
+	 * @throws AWTException
+	 * @Description Verify that audio document should be loaded successfully when
+	 *              document navigation done from Images tab of previous document.
+	 */
+
+	@Test(description = "RPMXCON-51691", dataProvider = "AllTheUsers", enabled = true, groups = { "regression" })
+	public void verifyAudioDocumentImagesTabInMiniDocList(String username, String password, String role)
+			throws InterruptedException {
+
+		baseClass.stepInfo("Test case Id: RPMXCON-51691");
+		baseClass.stepInfo(
+				"Verify that audio document should be loaded successfully when document navigation done from Images tab of previous document.");
+
+		DocViewPage docViewPage = new DocViewPage(driver);
+		sessionSearch = new SessionSearch(driver);
+		SoftAssert softAssertion = new SoftAssert();
+
+		// Login As user
+		loginPage.loginToSightLine(username, password);
+		baseClass.stepInfo("User successfully logged into slightline webpage as with " + username + "");
+
+		sessionSearch.verifyaudioSearchWarning(Input.audioSearchString1, Input.language);
+		sessionSearch.ViewInDocViews();
+
+		driver.waitForPageToBeReady();
+		baseClass.waitForElement(docViewPage.getDocView_ImagesTab());
+		docViewPage.getDocView_ImagesTab().waitAndClick(5);
+		if (baseClass.text("Images").isDisplayed()) {
+			baseClass.passedStep("In image tab it displayed");
+		} else {
+			baseClass.failedStep("There is no such message");
+		}
+		driver.waitForPageToBeReady();
+		baseClass.waitForElement(docViewPage.getDocView_Last());
+		docViewPage.getDocView_Last().waitAndClick(5);
+		driver.waitForPageToBeReady();
+		softAssertion.assertEquals(docViewPage.getDocView_TextFileType().getText().toString(), "MP3 VERSION");
+		baseClass.passedStep(
+				"Document is selected from mini doc list as per the clicked document navigation option and same audio document loaded in default view");
+		softAssertion.assertAll();
+		driver.waitForPageToBeReady();
+		baseClass.waitForElement(docViewPage.getDocView_TextTab());
+		docViewPage.getDocView_TextTab().waitAndClick(5);
+		if (baseClass.text("TEXT").isDisplayed()) {
+			baseClass.passedStep("Text document is be loaded on the respective tab Succesfully");
+		} else {
+			baseClass.failedStep("There is no such message");
+		}
+		driver.waitForPageToBeReady();
+		baseClass.waitForElement(docViewPage.getDocView_ImagesTab());
+		docViewPage.getDocView_ImagesTab().waitAndClick(5);
+		if (baseClass.text("Images").isDisplayed()) {
+			baseClass.passedStep("Images document is be loaded on the respective tab Succesfully");
+		} else {
+			baseClass.failedStep("There is no such message");
+		}
+		driver.waitForPageToBeReady();
+		baseClass.waitForElement(docViewPage.getDocView_TranslationTab());
+		docViewPage.getDocView_TranslationTab().waitAndClick(5);
+		if (baseClass.text("Translations").isDisplayed()) {
+			baseClass.passedStep("Translations document is be loaded on the respective tab Succesfully");
+		} else {
+			baseClass.failedStep("There is no such message");
+		}
+		// logout
+		loginPage.logout();
+	}
+
+	/**
+	 * @author Vijaya.Rani ModifyDate:08/12/2022 RPMXCON-51701
+	 * @throws InterruptedException
+	 * @throws AWTException
+	 * @DescriptionVerify that audio document should be loaded successfully when
+	 *                    document navigation done from Translations tab of previous
+	 *                    document
+	 */
+
+	@Test(description = "RPMXCON-51701", dataProvider = "AllTheUsers", enabled = true, groups = { "regression" })
+	public void verifyAudioDocumentTranslationsTabInMiniDocList(String username, String password, String role)
+			throws InterruptedException {
+
+		baseClass.stepInfo("Test case Id: RPMXCON-51701");
+		baseClass.stepInfo(
+				"Verify that audio document should be loaded successfully when document navigation done from Translations tab of previous document.");
+
+		DocViewPage docViewPage = new DocViewPage(driver);
+		sessionSearch = new SessionSearch(driver);
+		SoftAssert softAssertion = new SoftAssert();
+
+		// Login As user
+		loginPage.loginToSightLine(username, password);
+		baseClass.stepInfo("User successfully logged into slightline webpage as with " + username + "");
+
+		sessionSearch.verifyaudioSearchWarning(Input.audioSearchString1, Input.language);
+		sessionSearch.ViewInDocViews();
+
+		driver.waitForPageToBeReady();
+		baseClass.waitForElement(docViewPage.getDocView_TranslationTab());
+		docViewPage.getDocView_TranslationTab().waitAndClick(5);
+		if (baseClass.text("Translations").isDisplayed()) {
+			baseClass.passedStep("In translations tab it displayed");
+		} else {
+			baseClass.failedStep("There is no such message");
+		}
+		driver.waitForPageToBeReady();
+		baseClass.waitForElement(docViewPage.getDocView_Last());
+		docViewPage.getDocView_Last().waitAndClick(5);
+		driver.waitForPageToBeReady();
+		softAssertion.assertEquals(docViewPage.getDocView_TextFileType().getText().toString(), "MP3 VERSION");
+		baseClass.passedStep(
+				"Document is selected from mini doc list as per the clicked document navigation option and same audio document loaded in default view");
+		softAssertion.assertAll();
+		driver.waitForPageToBeReady();
+		baseClass.waitForElement(docViewPage.getDocView_TextTab());
+		docViewPage.getDocView_TextTab().waitAndClick(5);
+		if (baseClass.text("TEXT").isDisplayed()) {
+			baseClass.passedStep("Text document is be loaded on the respective tab Succesfully");
+		} else {
+			baseClass.failedStep("There is no such message");
+		}
+		driver.waitForPageToBeReady();
+		baseClass.waitForElement(docViewPage.getDocView_ImagesTab());
+		docViewPage.getDocView_ImagesTab().waitAndClick(5);
+		if (baseClass.text("Images").isDisplayed()) {
+			baseClass.passedStep("Images document is be loaded on the respective tab Succesfully");
+		} else {
+			baseClass.failedStep("There is no such message");
+		}
+		driver.waitForPageToBeReady();
+		baseClass.waitForElement(docViewPage.getDocView_TranslationTab());
+		docViewPage.getDocView_TranslationTab().waitAndClick(5);
+		if (baseClass.text("Translations").isDisplayed()) {
+			baseClass.passedStep("Translations document is be loaded on the respective tab Succesfully");
+		} else {
+			baseClass.failedStep("There is no such message");
+		}
+		// logout
+		loginPage.logout();
+	}
+
+	/**
+	 * Author : Vijaya.rani date: 8/12/22 RPMXCON-51112 Description:Verify user
+	 * after impersonation should be able to download the audio file which is
+	 * viewing in doc view.
+	 */
+	@Test(description = "RPMXCON-51112", enabled = true, groups = { "regression" })
+	public void verifyUsersImpersonateDownloadAudioFile() throws InterruptedException {
+
+		baseClass.stepInfo("Test case Id: RPMXCON-51112");
+		baseClass.stepInfo(
+				"Verify user after impersonation should be able to download the audio file which is viewing in doc view");
+
+		SessionSearch session = new SessionSearch(driver);
+		DocListPage docList = new DocListPage(driver);
+		// Login as SA
+		loginPage.loginToSightLine(Input.sa1userName, Input.sa1password);
+		baseClass.stepInfo("Successfully logged in as '" + Input.sa1userName + "'");
+		// impersonate SA to PA
+		baseClass.impersonateSAtoPA();
+		// Go to audioSearch
+		session.audioSearch(Input.audioSearchString1, Input.language);
+		session.viewInDocView();
+		driver.waitForPageToBeReady();
+		baseClass.waitForElement(docList.getAudioDownloadIcon());
+		baseClass.waitTillElemetToBeClickable(docList.getAudioDownloadIcon());
+		docList.getAudioDownloadIcon().Click();
+		baseClass.waitForElement(docList.getAudioOptionToDownload());
+		baseClass.waitTillElemetToBeClickable(docList.getAudioOptionToDownload());
+		docList.getAudioOptionToDownload().Click();
+		baseClass.waitUntilFileDownload();
+		String CompareString = "mp3";
+		String fileName = baseClass.GetFileName();
+		System.out.println(fileName);
+		baseClass.stepInfo(fileName);
+		driver.waitForPageToBeReady();
+		if (fileName.contains(CompareString)) {
+			baseClass.passedStep("Audio file is downloaded with the original format");
+		} else {
+			baseClass.failedStep("docs is not displayed as expected");
+		}
+		loginPage.logout();
+
+		// Login as SA
+		loginPage.loginToSightLine(Input.sa1userName, Input.sa1password);
+		baseClass.stepInfo("Successfully logged in as '" + Input.sa1userName + "'");
+		// impersonate SA to RMU
+		baseClass.impersonateSAtoRMU();
+		// Go to audioSearch
+		session.audioSearch(Input.audioSearchString1, Input.language);
+		session.viewInDocView();
+		driver.waitForPageToBeReady();
+		baseClass.waitForElement(docList.getAudioDownloadIcon());
+		baseClass.waitTillElemetToBeClickable(docList.getAudioDownloadIcon());
+		docList.getAudioDownloadIcon().Click();
+		baseClass.waitForElement(docList.getAudioOptionToDownload());
+		baseClass.waitTillElemetToBeClickable(docList.getAudioOptionToDownload());
+		docList.getAudioOptionToDownload().Click();
+		baseClass.waitUntilFileDownload();
+		driver.waitForPageToBeReady();
+		baseClass.stepInfo(fileName);
+		if (fileName.contains(CompareString)) {
+			baseClass.passedStep("Audio file is downloaded with the original format");
+		} else {
+			baseClass.failedStep("docs is not displayed as expected");
+		}
+		loginPage.logout();
+
+		// Login as SA
+		loginPage.loginToSightLine(Input.sa1userName, Input.sa1password);
+		baseClass.stepInfo("Successfully logged in as '" + Input.sa1userName + "'");
+		// impersonate SA to REV
+		baseClass.impersonateSAtoReviewer();
+		// Go to audioSearch
+		session.audioSearch(Input.audioSearchString1, Input.language);
+		session.viewInDocView();
+		driver.waitForPageToBeReady();
+		baseClass.waitForElement(docList.getAudioDownloadIcon());
+		baseClass.waitTillElemetToBeClickable(docList.getAudioDownloadIcon());
+		docList.getAudioDownloadIcon().Click();
+		baseClass.waitForElement(docList.getAudioOptionToDownload());
+		baseClass.waitTillElemetToBeClickable(docList.getAudioOptionToDownload());
+		docList.getAudioOptionToDownload().Click();
+		baseClass.waitUntilFileDownload();
+		baseClass.stepInfo(fileName);
+		driver.waitForPageToBeReady();
+		if (fileName.contains(CompareString)) {
+			baseClass.passedStep("Audio file is downloaded with the original format");
+		} else {
+			baseClass.failedStep("docs is not displayed as expected");
+		}
+		loginPage.logout();
+
+		// Login as PA
+		loginPage.loginToSightLine(Input.pa1userName, Input.pa1password);
+		baseClass.stepInfo("Successfully logged in as '" + Input.pa1userName + "'");
+		// impersonate PA to RMU
+		baseClass.impersonatePAtoRMU();
+		// Go to audioSearch
+		session.audioSearch(Input.audioSearchString1, Input.language);
+		session.viewInDocView();
+		driver.waitForPageToBeReady();
+		baseClass.waitForElement(docList.getAudioDownloadIcon());
+		baseClass.waitTillElemetToBeClickable(docList.getAudioDownloadIcon());
+		docList.getAudioDownloadIcon().Click();
+		baseClass.waitForElement(docList.getAudioOptionToDownload());
+		baseClass.waitTillElemetToBeClickable(docList.getAudioOptionToDownload());
+		docList.getAudioOptionToDownload().Click();
+		baseClass.waitUntilFileDownload();
+		driver.waitForPageToBeReady();
+		baseClass.stepInfo(fileName);
+		if (fileName.contains(CompareString)) {
+			baseClass.passedStep("Audio file is downloaded with the original format");
+		} else {
+			baseClass.failedStep("docs is not displayed as expected");
+		}
+		loginPage.logout();
+
+		// Login as PA
+		loginPage.loginToSightLine(Input.pa1userName, Input.pa1password);
+		baseClass.stepInfo("Successfully logged in as '" + Input.pa1userName + "'");
+		// impersonate PA to REV
+		baseClass.impersonatePAtoReviewer();
+		// Go to audioSearch
+		session.audioSearch(Input.audioSearchString1, Input.language);
+		session.viewInDocView();
+		driver.waitForPageToBeReady();
+		baseClass.waitForElement(docList.getAudioDownloadIcon());
+		baseClass.waitTillElemetToBeClickable(docList.getAudioDownloadIcon());
+		docList.getAudioDownloadIcon().Click();
+		baseClass.waitForElement(docList.getAudioOptionToDownload());
+		baseClass.waitTillElemetToBeClickable(docList.getAudioOptionToDownload());
+		docList.getAudioOptionToDownload().Click();
+		baseClass.waitUntilFileDownload();
+		baseClass.stepInfo(fileName);
+		driver.waitForPageToBeReady();
+		if (fileName.contains(CompareString)) {
+			baseClass.passedStep("Audio file is downloaded with the original format");
+		} else {
+			baseClass.failedStep("docs is not displayed as expected");
+		}
+		loginPage.logout();
+
+		// Login as RMU
+		loginPage.loginToSightLine(Input.rmu1userName, Input.rmu1password);
+		baseClass.stepInfo("Successfully logged in as '" + Input.rmu1userName + "'");
+		// impersonate RMU to REV
+		baseClass.impersonateRMUtoReviewer();
+		// Go to audioSearch
+		session.audioSearch(Input.audioSearchString1, Input.language);
+		session.viewInDocView();
+		driver.waitForPageToBeReady();
+		baseClass.waitForElement(docList.getAudioDownloadIcon());
+		baseClass.waitTillElemetToBeClickable(docList.getAudioDownloadIcon());
+		docList.getAudioDownloadIcon().Click();
+		baseClass.waitForElement(docList.getAudioOptionToDownload());
+		baseClass.waitTillElemetToBeClickable(docList.getAudioOptionToDownload());
+		docList.getAudioOptionToDownload().Click();
+		baseClass.waitUntilFileDownload();
+		baseClass.stepInfo(fileName);
+		driver.waitForPageToBeReady();
+		if (fileName.contains(CompareString)) {
+			baseClass.passedStep("Audio file is downloaded with the original format");
+		} else {
+			baseClass.failedStep("docs is not displayed as expected");
+		}
+		loginPage.logout();
+
+	}
+	
+	/**
+	 * @author Vijaya.Rani ModifyDate:08/12/2022 RPMXCON-51697
+	 * @throws InterruptedException
+	 * @throws AWTException
+	 * @Description Verify that audio document should be loaded successfully when document viewed from mini doc list from Images tab of previous document.
+	 */
+
+	@Test(description = "RPMXCON-51697", dataProvider = "AllTheUsers", enabled = true, groups = { "regression" })
+	public void verifyAudioDocumentImagesTabInMiniDocListDifferentView(String username, String password, String role)
+			throws InterruptedException {
+
+		baseClass.stepInfo("Test case Id: RPMXCON-51697");
+		baseClass.stepInfo(
+				"Verify that audio document should be loaded successfully when document viewed from mini doc list from Images tab of previous document.");
+
+		DocViewPage docViewPage = new DocViewPage(driver);
+		sessionSearch = new SessionSearch(driver);
+		SoftAssert softAssertion = new SoftAssert();
+
+		// Login As user
+		loginPage.loginToSightLine(username, password);
+		baseClass.stepInfo("User successfully logged into slightline webpage as with " + username + "");
+
+		sessionSearch.verifyaudioSearchWarning(Input.audioSearchString1, Input.language);
+		sessionSearch.ViewInDocViews();
+
+		driver.waitForPageToBeReady();
+		baseClass.waitForElement(docViewPage.getDocView_ImagesTab());
+		docViewPage.getDocView_ImagesTab().waitAndClick(5);
+		if (baseClass.text("Images").isDisplayed()) {
+			baseClass.passedStep("In image tab it displayed");
+		} else {
+			baseClass.failedStep("There is no such message");
+		}
+		driver.waitForPageToBeReady();
+		baseClass.waitForElement(docViewPage.getDocView_MiniDoc_Selectdoc(2));
+		docViewPage.getDocView_MiniDoc_Selectdoc(2).waitAndClick(5);
+		driver.waitForPageToBeReady();
+		softAssertion.assertEquals(docViewPage.getDocView_TextFileType().getText().toString(), "MP3 VERSION");
+		baseClass.passedStep(
+				"Document is selected from mini doc list as per the clicked document navigation option and same audio document loaded in default view");
+		softAssertion.assertAll();
+		driver.waitForPageToBeReady();
+		baseClass.waitForElement(docViewPage.getDocView_TextTab());
+		docViewPage.getDocView_TextTab().waitAndClick(5);
+		if (baseClass.text("TEXT").isDisplayed()) {
+			baseClass.passedStep("Text document is be loaded on the respective tab Succesfully");
+		} else {
+			baseClass.failedStep("There is no such message");
+		}
+		driver.waitForPageToBeReady();
+		baseClass.waitForElement(docViewPage.getDocView_ImagesTab());
+		docViewPage.getDocView_ImagesTab().waitAndClick(5);
+		if (baseClass.text("Images").isDisplayed()) {
+			baseClass.passedStep("Images document is be loaded on the respective tab Succesfully");
+		} else {
+			baseClass.failedStep("There is no such message");
+		}
+		driver.waitForPageToBeReady();
+		baseClass.waitForElement(docViewPage.getDocView_TranslationTab());
+		docViewPage.getDocView_TranslationTab().waitAndClick(5);
+		if (baseClass.text("Translations").isDisplayed()) {
+			baseClass.passedStep("Translations document is be loaded on the respective tab Succesfully");
+		} else {
+			baseClass.failedStep("There is no such message");
+		}
+		// logout
+		loginPage.logout();
+	}
+
+	@DataProvider(name = "AllTheUsers")
+	public Object[][] AllTheUsers() {
+		Object[][] users = { { Input.pa1userName, Input.pa1password, Input.pa1FullName },
+				{ Input.rmu1userName, Input.rmu1password, Input.rmu1FullName },
+				{ Input.rev1userName, Input.rev1password, Input.rev1FullName } };
+		return users;
+	}
+	
+	@AfterMethod(alwaysRun = true)
+	public void takeScreenShot(ITestResult result) {
+		Reporter.setCurrentTestResult(result);
+		if (ITestResult.FAILURE == result.getStatus()) {
+			Utility bc = new Utility(driver);
+			bc.screenShot(result);
+			System.out.println("Executed :" + result.getMethod().getMethodName());
+		}
+		try {
+			loginPage.quitBrowser();
+		} catch (Exception e) {
+			loginPage.quitBrowser();
+		}
+	}
+
+	@AfterClass(alwaysRun = true)
+	public void close() {
+		System.out.println("**Executed  DocList_Regression26.**");
+	}
+
+
+}
