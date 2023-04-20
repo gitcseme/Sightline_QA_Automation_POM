0f3631ca1f02b0386d3f7518f79ccd0a2dc0b4ca
Hemamadmin <Hemamadmin@LVS-W10-DEVS028.ConsilioTest.com>
2023-04-20 19:08:13 +0530

    pushing to git

--- sightline/src/main/java/sightline/docviewAnalyticsPanel/DocViewAnalyticsPanel_Regression2.java
+++ sightline/src/main/java/sightline/docviewAnalyticsPanel/DocViewAnalyticsPanel_Regression2.java
@@ -53,8 +53,8 @@ public class DocViewAnalyticsPanel_Regression2 {
 
 		System.out.println("******Execution started for " + this.getClass().getSimpleName() + "********");
 		// Open browser
-		Input in =new Input();
-		in.loadEnvConfig();
+//		Input in =new Input();
+//		in.loadEnvConfig();
 		driver = new Driver();
 		baseClass = new BaseClass(driver);
 		loginPage = new LoginPage(driver);
