c21b99ee1f7607215287360c38e5ae101447210f
Parvez Mahmud <parvez.mahmud@consilio.com>
2023-09-11 17:21:18 +0600

    method for token login

--- sightline/src/main/java/legalhold/legalholdpagefactory/login_logout/LoginWithToken.java
+++ sightline/src/main/java/legalhold/legalholdpagefactory/login_logout/LoginWithToken.java
@@ -1,4 +1,50 @@
 package legalhold.legalholdpagefactory.login_logout;
 
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+import static io.restassured.RestAssured.given;
+
 public class LoginWithToken {
+    WebDriver driver;
+
+    public void loginToLegalHoldWithToken() throws UnsupportedEncodingException {
+        String baseUrl = "https://LVSVQDSQLLH01:5656";
+
+        driver=new ChromeDriver();
+
+        RestAssured.useRelaxedHTTPSValidation();
+        RestAssured.baseURI = baseUrl;
+        Response response =
+                given()
+                        .contentType("application/json")
+                        .header("x-api-key", "vdFklU5xBN62IaCVqi0KCFi5mJ8pvLIcSd2AYhSqRXH9JCmUGOVIZUkOUxp8DIjX")
+                        .body("{\n" +
+                                "    \"userId\":\"7859\",\n" +
+                                "    \"role\":\"sysadmin\",\n" +
+                                "    \"tenantId\":\"854\",\n" +
+                                "    \"email\":\"syslegalhold@gmail.com\",\n" +
+                                "    \"FirstName\":\"System\",\n" +
+                                "    \"LastName\":\"Admin\"\n" +
+                                "}")
+                        .when()
+                        .post("/api/sso/request_token")
+                        .then().assertThat().statusCode(200).extract().response();
+
+        JsonPath resObj = response.jsonPath();
+        System.out.println(resObj);
+
+        String token = URLEncoder.encode(resObj.get("Token"), StandardCharsets.US_ASCII.toString());;
+
+
+        driver.get("https://legalholdqa.consiliotest.com" + "?token=" + token);
+    }
 }
